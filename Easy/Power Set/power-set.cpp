571e2acd8f3b079c4ec127ba9db7b3c56f9c22c9
Vishu Tyagi <93927043+jkvishu@users.noreply.github.com>
2023-01-03 23:32:58 +0530

    Added Solution - From GFG

---
+++ Easy/Power Set/power-set.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+	    static bool cmp(string a,string b){
+	        if(a[0]==b[0]){
+	            int i=0;
+	            while(a[i]==b[i])
+	            {
+	                i++;
+	                if(i<a.size()&&i<b.size()){
+	                    continue;
+	                }
+	                else{
+	                    return a.size()<b.size();
+	                }
+	            }
+	            return a[i]<b[i];
+	        }
+	        else{
+	            return a[0]<b[0];
+	        }
+	    }
+		vector<string> AllPossibleStrings(string s)
+		{
+		    // Code here
+		    int len=s.length();
+		    vector<string>v;
+		    string st="";
+		    long long int p=pow(2,len);
+		    for(int i=1;i<p;i++){
+		        for(int j=0;j<len;j++){
+		            if((i&(1<<j))!=0){
+		                st.push_back(s[j]);
+		            }
+		        }
+		        v.push_back(st);
+		        st="";
+		    }
+		    sort(v.begin(),v.end(),cmp);
+		    return v;
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string s;
+		cin >> s;
+		Solution ob;
+		vector<string> res = ob.AllPossibleStrings(s);
+		for(auto i : res)
+			cout << i <<" ";
+		cout << "\n";
+
+	}
+	return 0;
+}
+// } Driver Code Ends
