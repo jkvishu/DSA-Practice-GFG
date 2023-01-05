abed2f77c3b3deb8905018879fb8bd3d4ba180c6
Vishu Tyagi <93927043+jkvishu@users.noreply.github.com>
2023-01-05 12:14:38 +0530

    Added Solution - From GFG

---
+++ Basic/Gray to Binary equivalent/gray-to-binary-equivalent.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    // function to convert a given Gray equivalent n to Binary equivalent.
+    int grayToBinary(int n)
+    {
+        
+        // Your code here
+        //Done by my own logic but it's quite slow
+        if(n==0 || n==1)
+        return n;
+        string st="",kt="";
+        int l=1,ans=0;
+        while(n){
+            if((1&n)!=0){
+            st.push_back('1');
+        }
+        else{
+            st.push_back('0');
+        }
+        n=n>>1;
+    }
+    kt=st[st.size()-1];
+    int prev=st[st.size()-1]-'0';
+    for(int i=st.size()-2;i>=0;i--){
+        int xr=(st[i]-'0')^prev;
+        prev=xr;
+        kt+=to_string(xr);
+    }
+    for(int i=kt.size()-1;i>=0;i--){
+        if(kt[i]=='1'){
+            ans+=l;
+        }
+        l=l*2;
+    }
+    return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t,n;
+    cin>>t;//testcases
+    while(t--)
+    {
+        cin>>n;//initializing n
+        
+        Solution ob;
+        //calling function grayToBinary()
+       cout<< ob.grayToBinary(n)<<endl;
+    }
+}
+
+// } Driver Code Ends
