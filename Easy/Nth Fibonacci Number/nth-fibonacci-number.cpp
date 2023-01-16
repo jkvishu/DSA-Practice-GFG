88677ffaf055ca55fa7d172b72d49259dc751f65
Vishu Tyagi <93927043+jkvishu@users.noreply.github.com>
2023-01-16 19:35:31 +0530

    Added Solution - From GFG

---
+++ Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    long long int nthFibonacci(long long int n){
+        // code here
+        //Damn.. easy
+        if(n==0)
+        return 0;
+        if(n==1||n==2)
+        return 1;
+        long long int i=3,a=1,b=2,c=2,m=1000000007;
+        while(i!=n){
+            c=(a+b)%m;
+            a=b;
+            b=c;
+            i++;
+        }
+        return c%m;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.nthFibonacci(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
