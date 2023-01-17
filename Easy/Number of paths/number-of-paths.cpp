63ecefc5f988078c3f02d66b1e13d336b42b67a6
Vishu Tyagi <93927043+jkvishu@users.noreply.github.com>
2023-01-17 10:38:28 +0530

    Added Solution - From GFG

---
+++ Easy/Number of paths/number-of-paths.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+// } Driver Code Ends
+long long  numberOfPaths(int m, int n)
+{
+    // Code Here
+    //Done after something new cpycyt
+    if(m==1||n==1){
+            return 1;
+        }
+        return numberOfPaths(m-1,n)+numberOfPaths(m,n-1);
+}
+
+//{ Driver Code Starts.
+
+ 
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n,m;
+		cin>>m>>n;
+	    cout << numberOfPaths(m, n)<<endl;
+	}
+    return 0;
+}
+// } Driver Code Ends
