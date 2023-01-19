171d7994ab32399b80a96c80c1244aef6d072c16
Vishu Tyagi <93927043+jkvishu@users.noreply.github.com>
2023-01-19 21:45:38 +0530

    Added Solution - From GFG

---
+++ Basic/Repetitive Addition Of Digits/repetitive-addition-of-digits.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    int singleDigit(long long N)
+    {
+        // Write Your Code here
+        //One line best code available done by me on my own by my past experience
+        if(N%9==0)
+        return 9;
+        return int(N%9);
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long N;
+        cin>>N;
+        Solution ob;
+        int ans  = ob.singleDigit(N);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
