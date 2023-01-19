84aa26b13d86a6690f7ec26cf5bf60e5b932f002
Vishu Tyagi <93927043+jkvishu@users.noreply.github.com>
2023-01-19 18:28:59 +0530

    Update josephus-problem.cpp

--- Easy/Josephus problem/josephus-problem.cpp
+++ Easy/Josephus problem/josephus-problem.cpp
@@ -10,22 +10,41 @@ using namespace std;
 class Solution
 {
     public:
+    //SOLUTION 3:-> //PURE recursion
+    int hallfame(int n,int k){
+        if(n==1)
+        return 0;
+        else
+        return (hallfame(n-1,k)+k)%n;
+    }
+    int josephus(int n, int k){
+        return hallfame(n,k)+1;
+    }
+    /*
+    SOLUTION 2:->
+    int help(vector<int>&v,int n,int k,int i){
+        if(n==1){
+            return v[0];
+        }
+        i=(i+(k-1))%v.size();
+        v.erase(v.begin()+i);
+        help(v,n-1,k,i);
+    }
     int josephus(int n, int k)
     {
+        //Let's drive a recursive solution for the same.
         vector<int>v(n);
         for(int i=0;i<n;i++){
-            v[i]=(i+1);
+            v[i]=i+1;
         }
-        int i=0;
-        while(v.size()!=1){
-            i=(i+(k-1))%v.size();
-            v.erase(v.begin()+i);
-        }
-        return v[0];
-    }
-   /* {
+        return help(v,n,k,0);
+    }*/
+   /*
+   SOLUTION 1:->
+   {
        //Your code here
        //Done after help from yt
+       //This is iterative version of josephus problem.
        //Done by Vishu Tyagi
        int j;
        vector<int>arr(n);
