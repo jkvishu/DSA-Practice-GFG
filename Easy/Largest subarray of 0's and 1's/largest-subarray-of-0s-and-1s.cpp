3449a6539c8c3249bf13cc2f8bc3c7a7c65379ff
Vishu Tyagi <93927043+jkvishu@users.noreply.github.com>
2023-01-27 20:20:10 +0530

    Added Solution - From GFG

---
+++ Easy/Largest subarray of 0's and 1's/largest-subarray-of-0s-and-1s.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/*You are required to complete this method*/
+
+// arr[] : the input array containing 0s and 1s
+// N : size of the input array
+
+// return the maximum length of the subarray
+// with equal 0s and 1s
+class Solution{
+  public:
+    int maxLen(int arr[], int N)
+    {
+        // Your code here
+        //Done after getting a little idea from pepcoding.
+        //I Was extremely close to the same approach.
+        unordered_map<int,int>m;
+        int s=0,ans=0,c1=0,c0=0;
+        m[0]=-1;
+        for(int i=0;i<N;i++){
+            if(arr[i]==0)
+            {
+                s+=-1;
+                c0++;
+            }
+            else{
+                s+=+1;
+                c1++;
+            }
+            if(m.find(s)!=m.end()){
+                    ans=max(ans,i-m[s]);
+                }
+                else{
+                    m[s]=i;
+                }
+        }
+        if(c0==c1)
+        return N;
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    // your code goes here
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        Solution obj;
+        cout << obj.maxLen(a, n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
