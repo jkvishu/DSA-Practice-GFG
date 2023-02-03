968831ff3537c090b37edb8cd1a797ccf9dbd0cc
Vishu Tyagi <93927043+jkvishu@users.noreply.github.com>
2023-02-03 21:12:20 +0530

    Update bubble-sort.cpp

--- Easy/Bubble Sort/bubble-sort.cpp
+++ Easy/Bubble Sort/bubble-sort.cpp
@@ -26,11 +26,17 @@ class Solution
     {
         // Your code here 
         //easy boi 😂✅💯
+        int flag=0;
         for(int i=0;i<n-1;i++){
-            for(int j=1;j<n-i;j++){
-                if(arr[j]<arr[j-1])
-                swap(arr[j],arr[j-1]);
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                flag=1;
+                }
             }
+            if(flag==0)
+            break;
+            flag=0;
         }
     }
 };
