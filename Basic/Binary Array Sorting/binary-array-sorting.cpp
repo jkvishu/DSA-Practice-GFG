cc7f96d0938cb43eaa9b3b019e7d70070ee76a7e
Vishu Tyagi <93927043+jkvishu@users.noreply.github.com>
2023-02-07 21:05:06 +0530

    Update binary-array-sorting.cpp

--- Basic/Binary Array Sorting/binary-array-sorting.cpp
+++ Basic/Binary Array Sorting/binary-array-sorting.cpp
@@ -12,9 +12,10 @@ class Solution{
     // N: input array
     //Function to sort the binary array.
     void binSort(int A[], int N)
-    {
+    /*{
        //Your code here
        //✅💯😏
+       //1.First method single traversal.
        int i=-1,l=N;
        while(i<l){
            do{
@@ -27,10 +28,38 @@ class Solution{
            break;
            swap(A[i],A[l]);
        }
-       
-       /**************
-        * No need to print the array
-        * ************/
+    }*/
+    /*{
+        //Second method ✅💯
+        int i=0,c0=0,c1=0,j;
+        for(int i=0;i<N;i++){
+            if(A[i]==0)
+            c0++;
+            else
+            c1++;
+        }
+        i=j=0;
+        while(i<c0){
+            A[j++]=0;
+            i++;
+        }
+        i=0;
+        while(i<c1){
+            A[j++]=1;
+            i++;
+        }
+    }*/
+    {
+        //Third method.
+        //This is nice one ❌😏
+        int i=0, j=0;
+       while(j<N){
+           if(A[j]==0){ 
+               A[j]=1;
+               A[i++]=0;
+           }
+           j++;
+       }
     }
 };
 
