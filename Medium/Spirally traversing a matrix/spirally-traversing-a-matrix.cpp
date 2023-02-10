5674f5984a6cf7814d10284760e18275608c0354
Vishu Tyagi <93927043+jkvishu@users.noreply.github.com>
2023-02-10 22:27:30 +0530

    Added Solution - From GFG

---
+++ Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.cpp
@@ -0,0 +1,170 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{   
+    public: 
+    //Function to return a list of integers denoting spiral traversal of matrix.
+    vector<int> spirallyTraverse(vector<vector<int> > matrix, int r, int c) 
+    {
+        // code here
+        //✅💯😏
+        // My method or wa to solve the problem
+        // IF matrix mutation is allowed
+        int i=0,j=0,k=0;
+       vector<int> v;
+       while(1)
+       {
+           while(j<c&&matrix[i][j]!=-1)
+           {
+               k=1;
+               v.push_back(matrix[i][j]);
+               matrix[i][j]=-1;
+               j++;
+           }
+           j--;
+           i++;
+           while(i<r&&matrix[i][j]!=-1)
+           {
+               k=1;
+               v.push_back(matrix[i][j]);
+               matrix[i][j]=-1;
+               i++;
+           }
+           i--;
+           j--;
+           while(j>=0&&matrix[i][j]!=-1)
+           {
+               k=1;
+               v.push_back(matrix[i][j]);
+               matrix[i][j]=-1;
+               j--;
+           }
+           j++;
+           i--;
+           while(i>=0&&matrix[i][j]!=-1)
+           {
+               k=1;
+               v.push_back(matrix[i][j]);
+               matrix[i][j]=-1;
+               i--;
+           }
+           i++;
+           j++;
+           if(k==0)
+           {
+               break;
+           }
+           k=0;
+       }
+       return v;
+    }
+    /*{
+        //💯🤝
+        //IF matrix mutation is not allowed.
+        int left = 0;
+
+        int right = c-1;
+
+        int top = 0;
+
+        int bottom = r-1;
+
+        vector<int> ans;
+
+        while(left<=right && top<=bottom)
+
+        {
+
+            for(int i = left; i<=right ; i++)
+
+            {
+
+                ans.push_back(matrix[top][i]);
+
+            }
+
+            top++;
+
+            for(int i = top; i<=bottom ; i++)
+
+            {
+
+                ans.push_back(matrix[i][right]);
+
+            }
+
+            right--;
+
+            if(top<=bottom)
+
+            {
+
+                for(int i = right ; i>=left ; i--)
+
+                {
+
+                    ans.push_back(matrix[bottom][i]);
+
+                }
+
+                bottom--;
+
+            }
+
+            if(left<=right)
+
+            {
+
+                for(int i = bottom ; i>=top; i--)
+
+                {
+
+                    ans.push_back(matrix[i][left]);
+
+                }
+
+                left++;
+
+                
+
+            }
+
+        }
+
+        return ans;
+        
+    }*/
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--) 
+    {
+        int r,c;
+        cin>>r>>c;
+        vector<vector<int> > matrix(r); 
+
+        for(int i=0; i<r; i++)
+        {
+            matrix[i].assign(c, 0);
+            for( int j=0; j<c; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        vector<int> result = ob.spirallyTraverse(matrix, r, c);
+        for (int i = 0; i < result.size(); ++i)
+                cout<<result[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
