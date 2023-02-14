eddba84883a7c42a9d256bd199aba969dd7d7936
Vishu Tyagi <93927043+jkvishu@users.noreply.github.com>
2023-02-14 18:59:40 +0530

    Added Solution - From GFG

---
+++ Medium/Subarrays with equal 1s and 0s/subarrays-with-equal-1s-and-0s.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution{
+  public:
+    //Function to count subarrays with 1s and 0s.
+    long long int countSubarrWithEqualZeroAndOne(int arr[], int n)
+    {
+        //Your code here
+        //❌👀🤝
+        unordered_map<int,int>m;
+        int sum=0,c=0;
+        for(int i=0;i<n;i++){
+            m[sum]++;
+            if(arr[i])
+            sum++;
+            else
+            sum--;
+            c+=m[sum];
+        }
+        return c;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	int n,i;
+	cin>>n;
+	int arr[n];
+	for(i=0;i<n;i++)
+	cin>>arr[i];
+	Solution obj;
+	cout<< obj.countSubarrWithEqualZeroAndOne(arr, n)<<"\n";
+	}
+	return 0;
+}
+
+// } Driver Code Ends
