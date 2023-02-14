00748aecdf38a0179566dd5d573ba35219eb8895
Vishu Tyagi <93927043+jkvishu@users.noreply.github.com>
2023-02-15 00:48:10 +0530

    Updated Solution - From GFG

--- Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
+++ Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
@@ -7,9 +7,17 @@ using namespace std;
 // User function Template for C++
 class Solution {
   public:
-    long long int nthFibonacci(long long int n){
+    long long int nthFibonacci(long long int n)
+    {
         // code here
         //Damn.. easy
+        //Fastest method exsist on this earth
+        //Time complexity is O(logn)
+        if(n<=70){
+        double phi = (1 + sqrt(5)) / 2;
+        long long int ans= (round(pow(phi, n) / sqrt(5)));
+        return ans%1000000007;
+        }
         if(n==0)
         return 0;
         if(n==1||n==2)
