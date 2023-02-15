d4d729024da3e1f08baf05fa43f8d7a4fc887c4d
Vishu Tyagi <93927043+jkvishu@users.noreply.github.com>
2023-02-15 15:43:43 +0530

    Added Solution - From GFG

---
+++ Easy/Pattern searching/pattern-searching.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*
+str: string in which pattern we have to find pattern.
+text: pattern needs to searched.
+*/
+
+bool searchPattern(string str, string pat)
+{
+    // your code here
+    // ✅💯😏
+    int p=pat.length();
+    for(int i=0;i<str.length()-p+1;i++){
+        if(str.substr(i,p)==pat)
+        return 1;
+    }
+    return 0;
+}
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t; cin>>t;
+    
+    while(t--)
+    {
+        string str, pat;
+        cin>>str;
+        cin>>pat;
+        
+        if(searchPattern(str, pat) == true)
+            cout << "Present" <<endl;
+        else cout << "Not present" <<endl;
+    }
+}
+
+
+// } Driver Code Ends
