aab52564f1650f4c3ad4330a36e93f864e9235f8
Vishu Tyagi <93927043+jkvishu@users.noreply.github.com>
2023-02-20 13:01:00 +0530

    Added Solution - From GFG

---
+++ Easy/Insert in a Sorted List/insert-in-a-sorted-list.cpp
@@ -0,0 +1,105 @@
+//{ Driver Code Starts
+//
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+}
+
+// } Driver Code Ends
+/*
+structure of the node of the list is as
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution{
+  public:
+    // Should return head of the modified linked list
+    Node *sortedInsert(struct Node* head, int x) {
+        // Code here
+        // Jod ✅💯😏
+        Node* s1=head;
+        Node* temp=head;
+        if(head->data>x){
+            Node* ax=new Node(x);
+            ax->next=head;
+            head=ax;
+            return head;
+        }
+        while(s1!=NULL){
+            if(s1->data>x){
+                Node* temp1=new Node(x);
+                temp->next=temp1;
+                temp1->next=s1;
+                return head;
+            }
+            temp=s1;
+            s1=s1->next;
+        }
+        Node* temp1=new Node(x);
+        temp->next=temp1;
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+        
+		int data;
+		cin>>data;
+		
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		for (int i = 0; i < n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+		}
+		
+		int k;
+		cin>>k;
+		Solution obj;
+		head = obj.sortedInsert(head,k);
+		printList(head); 
+	}
+	return 0; 
+} 
+
+// } Driver Code Ends
