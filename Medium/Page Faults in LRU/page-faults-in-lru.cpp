71c53e54e8e4ce1af58b8ee58729af9345111c18
Vishu Tyagi <www.vishutyagi8@gmail.com>
2023-02-28 13:26:07 +0530

    Added Solution - From GFG

---
+++ Medium/Page Faults in LRU/page-faults-in-lru.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int pageFaults(int N, int C, int pages[]){
+        // code here
+        // ✅🤝👀
+        int ans =0;
+        vector<int>v;
+        for(int i=0;i<N;i++){
+            auto it = find(v.begin(),v.end(),pages[i]);
+            if(it==v.end()){
+                ans++;
+                if(v.size()==C){
+                    v.erase(v.begin());
+                }
+                v.push_back(pages[i]);
+            }
+            else{
+                v.erase(it);
+                v.push_back(pages[i]);
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N, C;
+        cin>>N;
+        int pages[N];
+        for(int i = 0;i < N;i++)
+            cin>>pages[i];
+        cin>>C;
+        
+        Solution ob;
+        cout<<ob.pageFaults(N, C, pages)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
