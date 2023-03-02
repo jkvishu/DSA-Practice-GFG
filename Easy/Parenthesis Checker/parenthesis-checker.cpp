90ec3872592a55ce9f52bae7aef02a93a198707e
Vishu Tyagi <www.vishutyagi8@gmail.com>
2023-03-02 13:38:51 +0530

    Added Solution - From GFG

---
+++ Easy/Parenthesis Checker/parenthesis-checker.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to check if brackets are balanced or not.
+    bool ispar(string x)
+    {
+        // Your code here
+        // ✅💯😏
+        stack<char>s;
+        for(int i=0;i<x.length();i++){
+            if((x[i]=='{')||(x[i]=='(')||(x[i]=='[')){
+                s.push(x[i]);
+            }
+            else{
+                if(!s.empty()){
+                    if(x[i]=='}'){
+                        if(s.top()=='{')
+                        s.pop();
+                        else
+                        return 0;
+                    }
+                    else if(x[i]==')'){
+                        if(s.top()=='(')
+                        s.pop();
+                        else
+                        return 0;
+                    }
+                    else{
+                        if(s.top()=='[')
+                        s.pop();
+                        else
+                        return 0;
+                    }
+                }
+                else{
+                    return 0;
+                }
+            }
+        }
+        if(s.empty())
+        return 1;
+        return 0;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+   int t;
+   string a;
+   cin>>t;
+   while(t--)
+   {
+       cin>>a;
+       Solution obj;
+       if(obj.ispar(a))
+        cout<<"balanced"<<endl;
+       else
+        cout<<"not balanced"<<endl;
+   }
+}
+// } Driver Code Ends
