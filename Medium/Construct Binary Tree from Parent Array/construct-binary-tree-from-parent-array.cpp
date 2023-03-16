61dedf5384ed02f5e2df5701613637620a1f0143
Vishu Tyagi <www.vishutyagi8@gmail.com>
2023-03-16 19:34:44 +0530

    Added Solution - From GFG

---
+++ Medium/Construct Binary Tree from Parent Array/construct-binary-tree-from-parent-array.cpp
@@ -0,0 +1,137 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+
+void sort_and_print(vector <int> &v)
+{
+    sort( v.begin() , v.end() );
+    for( int i=0 ; i<v.size() ; i++ )
+        cout<< v[i] << " ";
+    v.clear();
+}
+
+void printLevelOrder(struct Node* root)
+{
+    vector <int> v;
+    queue <Node*> q;
+    
+    q.push(root);
+    
+    Node* next_row = NULL;
+    
+    while(!q.empty())
+    {
+        Node* n = q.front();
+        q.pop();
+        
+        if(n==next_row)
+        {
+            sort_and_print(v);
+            next_row = NULL;
+        }
+        
+        v.push_back(n->data);
+        
+        if(n->left)
+        {
+            q.push(n->left);
+            if(next_row == NULL) next_row = n->left;
+        }
+        
+        if(n->right)
+        {
+            q.push(n->right);
+            if(next_row == NULL) next_row = n->right;
+        }
+    }
+    sort_and_print(v);
+}
+
+Node *createTree(int parent[], int n);
+
+/* Driver program to test above function*/
+
+
+// } Driver Code Ends
+/* node structure  used in the program
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+
+class Solution{
+  public:
+    //Function to construct binary tree from parent array.
+    Node *createTree(int pa[], int N)
+    {
+        // Your code here
+        // TO much tricky and frustrating solution 
+        // ❌🥵👀©🤝
+        map<int,Node*>m;
+        Node* ans=NULL;
+        for(int i=0;i<N;i++){
+            Node* st=new Node(i);
+            m[i]=st;
+        }
+        for(int i=0;i<N;i++){
+            if(pa[i]==-1){
+                ans=m[i];
+            }
+            else if(m[pa[i]]->left==NULL){
+                m[pa[i]]->left=m[i];
+            }
+            else if(m[pa[i]]->right==NULL){
+                m[pa[i]]->right=m[i];
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>> t;
+    while(t--)
+    {
+        int n;
+        cin>> n;
+        int a[n];
+        for( int i=0 ; i<n ; i++ )
+            cin>> a[i];
+            
+        Solution ob;
+        
+        Node *res = ob.createTree(a,n);
+        
+        printLevelOrder(res);
+        cout<<endl;
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
