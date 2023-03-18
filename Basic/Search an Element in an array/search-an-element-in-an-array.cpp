117d3ccfe43866452d265222a9a12376f8ed3526
Vishu Tyagi <www.vishutyagi8@gmail.com>
2023-03-18 19:40:18 +0530

    Added Solution - From GFG

---
+++ Basic/Search an Element in an array/search-an-element-in-an-array.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // Function to search x in arr
+    // arr: input array
+    // X: element to be searched for
+    int search(int arr[], int N, int X)
+    {
+        // Your code here
+        // ✅💯😏
+        int i=0;
+        while(i<N){
+            if(arr[i]==X)
+            return i;
+            i++;
+        }
+        return -1;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int testcases;
+    cin>>testcases;
+    while(testcases--)
+    {
+        int sizeOfArray;
+        cin>>sizeOfArray;
+        int arr[sizeOfArray];
+        int x;
+        
+        for(int i=0;i<sizeOfArray;i++)
+        {
+            cin>>arr[i];
+        }
+        cin>>x;
+        Solution ob;
+        cout<<ob.search(arr,sizeOfArray,x)<<endl; //Linear search
+    }
+
+    return 0;
+    
+}
+
+// } Driver Code Ends
