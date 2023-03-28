3c6473b4d3e516364f0f31f392e1b09f2ba2f72f
Vishu Tyagi <www.vishutyagi8@gmail.com>
2023-03-29 00:50:47 +0530

    Update count-the-substrings.cpp

--- Easy/Count the Substrings/count-the-substrings.cpp
+++ Easy/Count the Substrings/count-the-substrings.cpp
@@ -26,7 +26,25 @@ class Solution
             c1=c2=0;
         }
         return ans;
-    }
+    }/*
+    //Compromising space and having good time complexity
+    //Just for knowledge
+        int n = S.size();
+        vector<int> a(n);
+        for(int i=0; i<n; i++) {
+            if(S[i]>='A' && S[i]<='Z') a[i] = 1;
+            else a[i] = -1;
+        }
+        map<int, int> mp;
+        int sum = 0, ans = 0;
+        mp[0] = 1;
+        for(auto& x: a) {
+            sum += x;
+            ans += mp[sum];
+            ++mp[sum];
+        }
+        return ans;
+    */
 };
 
 //{ Driver Code Starts.
