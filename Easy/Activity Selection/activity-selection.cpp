f709b79dd51535de4713455215b98bfeae8271c7
Vishu Tyagi <www.vishutyagi8@gmail.com>
2023-03-28 21:52:18 +0530

    Added Solution - From GFG

---
+++ Easy/Activity Selection/activity-selection.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum number of activities that can
+    //be performed by a single person.
+    int activitySelection(vector<int> start, vector<int> end, int n)
+    {
+        // Your code here
+        // ✅💯😏
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        for(int i=0;i<n;i++){
+            pq.push({end[i],start[i]});
+        }
+        int ans=1,l=pq.top().first;
+        pq.pop();
+        while(!pq.empty()){
+            if(pq.top().second>l){
+                ans++;
+                l=pq.top().first;
+            }
+            pq.pop();
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    
+    //testcases
+    cin >> t;
+    while(t--)
+    {
+        //size of array
+        int n;
+        cin >> n;
+        vector<int> start(n), end(n);
+        
+        //adding elements to arrays start and end
+        for(int i=0;i<n;i++)
+            cin>>start[i];
+        for(int i=0;i<n;i++)
+            cin>>end[i];
+        Solution ob;
+        //function call
+        cout << ob.activitySelection(start, end, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
