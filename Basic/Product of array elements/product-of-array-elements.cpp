ae6bfac3fad0c26dbbc41cde4c12ef2c121ba5d7
Vishu Tyagi <www.vishutyagi8@gmail.com>
2023-04-02 19:23:53 +0530

    Added Solution - From GFG

---
+++ Basic/Product of array elements/product-of-array-elements.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+
+/*  
+     ar[] is the array
+     n = number of element in array
+     mod = modulo value;
+*/
+long long int product(int ar[], int n, long long int mod)
+ {
+     
+     //code here
+     //✅💯😏
+     long long ans=1;
+     int i=0;
+     while(i<n){
+         ans=ans*ar[i]%mod;
+         i++;
+     }
+     return ans;
+     
+ }
+
+//{ Driver Code Starts.
+
+int main()
+{
+ int t;
+ cin>>t;
+ while(t--)
+   {
+    int n;
+    cin>>n;
+    int ar[100001]={0};
+    for(int i=0;i<n;i++)
+      cin>>ar[i];  
+      
+    long long int mod=1000000007;
+   cout<<product(ar,n,mod)<<endl;
+    
+ }
+return 0;
+}
+// } Driver Code Ends
