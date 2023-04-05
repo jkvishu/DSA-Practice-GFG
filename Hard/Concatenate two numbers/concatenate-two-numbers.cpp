d8f775b5fc50e0208c67d08a8bdeb45cd4b520a9
Vishu Tyagi <www.vishutyagi8@gmail.com>
2023-04-05 21:19:51 +0530

    Added Solution - From GFG

---
+++ Hard/Concatenate two numbers/concatenate-two-numbers.cpp
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution{
+    public:
+    long long countPairs(int N, int X, vector<int> numbers){
+        // code here
+        // 👀©❌
+         int n=numbers.size();
+
+        map<string ,int>mp;
+
+        for(int i=0;i<n;i++){
+
+            string s=to_string(numbers[i]);
+
+            mp[s]++;
+
+            
+
+        }
+
+        string s2=to_string(X);
+
+        int m=s2.size();
+
+        int idx=1;
+
+        int cnt=0;
+
+        for(int i=0;i<m;i++){
+
+           string x1=s2.substr(0,i+1);
+
+           string x2=s2.substr(i+1,m-i-1);
+
+           if(mp[x1]&&mp[x2]){
+
+            if(x1!=x2){cnt+=(mp[x1]*mp[x2]);}
+
+            else{
+
+              cnt+=((mp[x1]-1)*mp[x1]);    
+
+                
+
+            }
+
+           }
+
+        }
+
+        return cnt;
+
+ 
+    }
+}; 
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;cin>>t;
+    while(t--)
+    {
+        int N,X;
+        cin>>N>>X;
+        vector<int> numbers(N);
+        for(int i=0;i<N;i++)
+            cin>>numbers[i];
+        Solution ob;
+        long long ans = ob.countPairs(N, X, numbers);
+        cout<<ans<<endl;
+        
+    }
+
+    return 0; 
+}
+// } Driver Code Ends
