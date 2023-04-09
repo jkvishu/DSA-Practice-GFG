803a9d571be8d0788a9ca6c101ec25d8b8291221
Vishu Tyagi <www.vishutyagi8@gmail.com>
2023-04-09 13:47:52 +0530

    Update unit-area-of-largest-region-of-1s.cpp

--- Medium/Unit Area of largest region of 1's/unit-area-of-largest-region-of-1s.cpp
+++ Medium/Unit Area of largest region of 1's/unit-area-of-largest-region-of-1s.cpp
@@ -7,7 +7,7 @@ class Solution
 {
     public:
     //Function to find unit area of the largest region of 1s.
-    void dfs(int i,int j,vector<vector<int>>& grid,int n,int m,int &c){
+    /*void dfs(int i,int j,vector<vector<int>>& grid,int n,int m,int &c){
         if(i>=n||i<0||j>=m||j<0||grid[i][j]==0)
         return;
         else{
@@ -26,6 +26,8 @@ class Solution
     int findMaxArea(vector<vector<int>>& grid) {
         // Code here
         // ✅💯😏😎
+        // Time taken 0.29sec
+        // if mutation of the matrix is allowed.
         int ans=INT_MIN;
         int n=grid.size();
         int m=grid[0].size();
@@ -38,6 +40,40 @@ class Solution
             }
         }
         return ans;
+    }*/
+    void dfs(int row, int col, vector<vector<int>> &grid, vector<vector<int>> &vis, int &count){
+        vis[row][col] = 1;
+        count++;
+        int n = grid.size();
+        int m = grid[0].size();
+        for(int i=-1; i<=1; i++){
+            for(int j=-1; j<=1; j++){
+                int nrow = row+i;
+                int ncol = col+j;
+                if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && !vis[nrow][ncol] && grid[nrow][ncol] == 1){
+                    dfs(nrow, ncol, grid, vis, count);
+                }
+            }
+        }
+    }
+    int findMaxArea(vector<vector<int>>& grid) {
+        //IF mutation of the matrix is not allowed.
+        //✅💯🤝😏
+        //Time taken 0.33sec
+        int n = grid.size();
+        int m = grid[0].size();
+        int maxArea = 0;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(!vis[i][j] && grid[i][j] == 1){
+                    int area = 0;
+                    dfs(i, j, grid, vis, area);
+                    maxArea = max(maxArea, area);
+                }
+            }
+        }
+        return maxArea;
     }
 };
 
