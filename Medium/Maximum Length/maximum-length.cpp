21f5acdb2cc9656626ce4be9fdb2c4bd01b56ee8
Vishu Tyagi <www.vishutyagi8@gmail.com>
2023-04-11 11:43:10 +0530

    Added Solution - From GFG

---
+++ Medium/Maximum Length/maximum-length.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+public:
+    int solve(int a, int b, int c) {
+        // code here
+        // ❌©©👀
+        // POTD
+        int mx=max(a,max(b,c));
+        if(mx>2*(a+b+c+1-mx))
+        return -1;
+        else
+        return a+b+c;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        Solution ob;
+        int ans = ob.solve(a, b, c);
+     
+
+        cout <<  ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
