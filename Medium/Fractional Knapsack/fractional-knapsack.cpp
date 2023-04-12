8adacadd5b2c08bee56fbb51c9e4cbc572fc47e4
Vishu Tyagi <www.vishutyagi8@gmail.com>
2023-04-12 09:48:34 +0530

    Added Solution - From GFG

---
+++ Medium/Fractional Knapsack/fractional-knapsack.cpp
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Item{
+    int value;
+    int weight;
+};
+
+
+// } Driver Code Ends
+//class implemented
+/*
+struct Item{
+    int value;
+    int weight;
+};
+*/
+
+
+class Solution
+{
+    public:
+    //Function to get the maximum total value in the knapsack.
+    double fractionalKnapsack(int W, Item arr[], int n)
+    {
+        // Your code here
+        // BRute force approach but works completely fine
+        // ✅💯😏
+        priority_queue<pair<double,pair<int,int>>>pq;
+        for(int i=0;i<n;i++){
+            pq.push({(double)arr[i].value/arr[i].weight,{arr[i].value,arr[i].weight}});
+        }
+        double ans=0.0;
+        if(pq.top().second.second<W){
+            W=W-pq.top().second.second;
+            ans+=pq.top().second.first;
+        }
+        else{
+            ans+=(pq.top().second.first)*((double)W/pq.top().second.second);
+            W=0;
+        }
+        pq.pop();
+        while(!pq.empty()&&W){
+             if(pq.top().second.second<W){
+            W=W-pq.top().second.second;
+            ans+=pq.top().second.first;
+        }
+        else{
+            ans+=(pq.top().second.first)*((double)W/pq.top().second.second);
+            W=0;
+        }
+        pq.pop();
+        }
+        return ans;
+    }
+        
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	//taking testcases
+	cin>>t;
+	cout<<setprecision(2)<<fixed;
+	while(t--){
+	    //size of array and weight
+		int n, W;
+		cin>>n>>W;
+		
+		Item arr[n];
+		//value and weight of each item
+		for(int i=0;i<n;i++){
+			cin>>arr[i].value>>arr[i].weight;
+		}
+		
+		//function call
+		Solution ob;
+		cout<<ob.fractionalKnapsack(W, arr, n)<<endl;
+	}
+    return 0;
+}
+// } Driver Code Ends
