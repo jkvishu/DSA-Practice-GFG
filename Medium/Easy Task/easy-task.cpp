22e39cecb9be7b8494c11d5e8e5cbf2e5362f2e2
Vishu Tyagi <www.vishutyagi8@gmail.com>
2023-04-27 10:18:31 +0530

    Added Solution - From GFG

---
+++ Medium/Easy Task/easy-task.cpp
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    vector<char> easyTask(int n,string s,int q,vector<vector<string>> &queries){
+        // Code here
+        // POTD ❌©👀
+        vector<char> v;
+      
+      for(int i=0;i<q;i++) {
+          
+          if(queries[i][0]=="1") {
+              int index=stoi(queries[i][1]);
+               s[index]=queries[i][2][0];
+          }
+          else {
+              
+              int start=stoi(queries[i][1]);
+              int end=stoi(queries[i][2]);
+              int k=stoi(queries[i][3]);
+             
+             int freq[26]={0};
+                 int count=0 ;
+                 
+             for(int j=start;j<=end;j++ ) {
+                 freq[s[j]-'a']++;
+             }
+             
+             for(int j=25;j>=0;j--) {
+                 count+=freq[j];
+                 if(count>=k)
+                 {
+                     count=j+'a';
+                     break;
+                 }
+             }
+            
+              v.push_back(count);
+        
+          }
+      }
+    
+      return v;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+        int q;
+        cin>>q;
+        vector<vector<string>> queries(q);
+        for(int i=0;i<q;i++){
+            string x;
+            cin>>x;
+            if(x=="1"){
+                string p,q;
+                cin>>p>>q;
+                queries[i]={"1",p,q};
+            }
+            else{
+                string p,q,r;
+                cin>>p>>q>>r;
+                queries[i]={"2",p,q,r};
+            }
+        }
+        Solution ob;
+        vector<char> ans=ob.easyTask(n,s,q,queries);
+        for(auto ch:ans){
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+// } Driver Code Ends
