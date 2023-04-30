2119310416d6722bc80651625389ebabf1fb5fbd
Vishu Tyagi <www.vishutyagi8@gmail.com>
2023-04-30 10:23:54 +0530

    Added Solution - From GFG

---
+++ Medium/Egg Dropping Puzzle/egg-dropping-puzzle.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find minimum number of attempts needed in 
+    //order to find the critical floor.
+    int eggDrop(int n, int k) 
+    {
+        // your code here
+        // Dp most important and most asked question
+        // VVI ⭐️❤️🥵
+        // ✅©🤝🤝👀
+        int dp[k+1][n+1];
+        for(int i=1;i<=n;i++){
+            dp[1][i]=1;
+            dp[0][i]=0;
+        }
+        for(int i=1;i<=k;i++){
+            dp[i][1]=i;
+        }
+        for(int i=2;i<=k;i++){
+            for(int j=2;j<=n;j++){
+                dp[i][j]=INT_MAX;
+                for(int x=1;x<=i;x++){
+                    dp[i][j]=min(dp[i][j],1+max(dp[x-1][j-1],dp[i-x][j]));
+                }
+            }
+        }
+        return dp[k][n];
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //taking eggs and floors count
+        int n, k;
+        cin>>n>>k;
+        Solution ob;
+        //calling function eggDrop()
+        cout<<ob.eggDrop(n, k)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
