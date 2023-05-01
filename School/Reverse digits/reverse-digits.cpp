98a491eda87a6eaee15606d0410864ae5fb6217b
Vishu Tyagi <www.vishutyagi8@gmail.com>
2023-05-01 22:52:07 +0530

    Added Solution - From GFG

---
+++ School/Reverse digits/reverse-digits.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+	public:
+		long long int reverse_digit(long long int n)
+		{
+		    // Code here
+		    long long int ans=0,k=10,m=n;
+		    while(m){
+		        int r=m%10;
+		        ans=ans*k+r;
+		        m=m/10;
+		    }
+		    return ans;
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	long long int n;
+    	cin >> n;
+    	Solution ob;
+    	long long int  ans = ob.reverse_digit(n);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
