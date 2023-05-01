88ebb12ae6b7bda73e9d2dc3297b79c42323c15b
Vishu Tyagi <www.vishutyagi8@gmail.com>
2023-05-01 20:45:32 +0530

    Added Solution - From GFG

---
+++ School/Palindrome/palindrome.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+	public:
+		string is_palindrome(int n)
+		{
+		    // Code here.
+		    int k=10;
+		    int ans=0;
+		    int m=n;
+		    while(m>0){
+		        int r=m%10;
+		        ans=(ans*k)+r;
+		        m=m/10;
+		    }
+		    if(ans==n)
+		    return "Yes";
+		    return "No";
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	Solution ob;
+    	string ans = ob.is_palindrome(n);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
