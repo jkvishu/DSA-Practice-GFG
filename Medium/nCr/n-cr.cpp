1a893b4f3a67e764b809fd999daaa3071a1d2d8a
Vishu Tyagi <www.vishutyagi8@gmail.com>
2023-05-06 17:03:07 +0530

    Added Solution - From GFG

---
+++ Medium/nCr/n-cr.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int nCr(int n, int k){
+      // Code here
+      // ✅💯👀😏©👀
+    int mod = 1e9 + 7;   
+    int a[n + 1][k + 1];
+    
+    if (n == k)
+        return 1;
+        
+    if(n < k)
+        return 0;
+
+    for (int i = 0; i <= n; i++){
+        for (int j = 0; j <= k; j++){
+            
+            if (i == j || j == 0)
+                a[i][j] = 1;
+
+            else
+                a[i][j] =(a[i - 1][j] % mod) + (a[i - 1][j - 1] % mod) % mod;
+        }
+    }
+
+    return (a[n][k] % mod);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, r;
+        cin>>n>>r;
+        
+        Solution ob;
+        cout<<ob.nCr(n, r)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
