0f05f9847690cf5bfb27acc93cbc2e877acee35e
Vishu Tyagi <www.vishutyagi8@gmail.com>
2023-05-15 17:54:55 +0530

    Added Solution - From GFG

---
+++ Basic/Pattern 6/pattern-6.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+	
+	void printTriangle(int n) {
+	    // code here
+	    while(n){
+	        for(int i=1;i<=n;i++)
+	        cout<<i<<" ";
+	        cout<<endl;
+	        n--;
+	    }
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
