07eb6c34a1b35beaa5e38e009b48009e3984afe5
Vishu Tyagi <www.vishutyagi8@gmail.com>
2023-06-10 12:37:54 +0530

    Update arranging-the-array.cpp

--- Medium/Arranging the array/arranging-the-array.cpp
+++ Medium/Arranging the array/arranging-the-array.cpp
@@ -28,6 +28,73 @@ class Solution
                 arr[k++]=ps[i];
             }
         }
+        /*
+        //Optimal approach is this
+        //©👀🙃
+        public void swap(int arr[], int i, int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    
+    
+    
+    public void merge(int start, int mid , int end, int arr[]){
+        
+        int i = start; 
+        int j = mid + 1; 
+ 
+        while (i <= mid && arr[i] < 0)
+            i++;
+ 
+ 
+        while (j <= end && arr[j] < 0)
+            j++;
+        
+        // reverse positive part of the left array 
+        reverse(arr, i, mid);
+        
+        // reverse negative part of the right array
+        reverse(arr,mid+1, j-1);
+        
+        // reverse complete from start of positive in left to the end of the negative in right
+        reverse(arr, i, j-1);
+        
+    }
+    
+    public void helper(int start, int end, int arr[]){
+        if(end-start==1){
+            if(arr[start]>=0 && arr[end]<0) swap(arr,start, end);
+            return ;
+        }
+        else if(start==end){
+            return ;
+        }
+        else if(start<end){
+            int mid= (end-start)/2 +start;
+            helper(start, mid, arr);
+            helper(mid+1, end, arr);
+            merge(start,mid,end,arr);
+        }
+    }
+    
+
+    
+    public void Rearrange(int a[], int N)
+    {
+        
+        helper(0, N-1, a);
+    }
+    
+    void reverse(int arr[], int start, int end){
+        while(start<end){
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
+}
+        */
 };
 
 //{ Driver Code Starts.
