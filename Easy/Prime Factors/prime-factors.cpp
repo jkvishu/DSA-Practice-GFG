f5fd467f1de4885176187128071ebcb4c511fb74
Vishu Tyagi <www.vishutyagi8@gmail.com>
2023-07-01 13:59:35 +0530

    Updated Solution - From GFG

--- Easy/Prime Factors/prime-factors.cpp
+++ Easy/Prime Factors/prime-factors.cpp
@@ -9,6 +9,49 @@ using namespace std;
 
 class Solution{
 	public:
+	vector<int>AllPrimeFactors(int N) {
+	    // Code here
+	    // Just practicing again before placement
+	    vector<int>ans;
+	    unordered_set<int>s;
+	    if(N==1)
+	    return {};
+	    while(N%2==0){
+	        if(s.find(2)==s.end()){
+	        ans.push_back(2);
+	        s.insert(2);
+	        }
+	        N=N/2;
+	    }
+	    while(N%3==0){
+	        if(s.find(3)==s.end()){
+	        ans.push_back(3);
+	        s.insert(3);
+	        }
+	        N=N/3;
+	    }
+	    for(int i=5;i<N/2;i=i+6){
+	        while(N%i==0){
+	            if(s.find(i)==s.end()){
+	            ans.push_back(i);
+	            s.insert(i);
+	            }
+	            N=N/i;
+	        }
+	        while(N%(i+2)==0){
+	            if(s.find(i+2)==s.end()){
+	            ans.push_back(i+2);
+	            s.insert(i+2);
+	            }
+	            N=N/(i+2);
+	        }
+	    }
+	    if(N>3)
+	    ans.push_back(N);
+	    return ans;
+	}
+	/*
+	// PRevious submission
 	vector<int>AllPrimeFactors(int N) 
 	{
 	    // Code here
@@ -48,6 +91,7 @@ class Solution{
 	    sort(v.begin(),v.end());
 	    return v;
 	}
+	*/
 };
 
 //{ Driver Code Starts.
