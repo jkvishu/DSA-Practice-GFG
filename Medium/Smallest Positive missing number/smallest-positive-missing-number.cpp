11c418bdc5e59ac4874a36e8b298129ab833e126
Vishu Tyagi <www.vishutyagi8@gmail.com>
2023-07-09 19:13:17 +0530

    Updated Solution - From GFG

--- Medium/Smallest Positive missing number/smallest-positive-missing-number.cpp
+++ Medium/Smallest Positive missing number/smallest-positive-missing-number.cpp
@@ -9,10 +9,11 @@ class Solution
 {
     public:
     //Function to find the smallest positive number missing from the array.
-    int missingNumber(int arr[], int n) 
+    /*int missingNumber(int arr[], int n) 
     { 
         // Your code here
         // POTD ✅😏💯
+        // Very much brute force approach.
         int mx=INT_MIN;
         unordered_set<int>m;
         for(int i=0;i<n;i++){
@@ -26,7 +27,23 @@ class Solution
         if(mx<=0)
         return 1;
         return mx+1;
-    } 
+    } */
+    int missingNumber(int arr[], int n) 
+    { 
+        //POTD ✅😏💯
+        //MOst Optimal approach.
+         for(int i =0 ;i <n ;i++) {
+            while(arr[i] > 0 && arr[i] <=n && arr[i] != arr[arr[i] -1]) {
+                swap(arr[i] , arr[arr[i]-1]);
+            }
+        }
+        for(int i = 0; i <n;i++) {
+            if(arr[i] != i+1) {
+                return i+1;
+            }
+        }
+        return n+1;
+    }
 };
 
 //{ Driver Code Starts.
