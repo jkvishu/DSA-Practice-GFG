e9533e6d7d68f89cf2d4d21a18440db30599a789
Vishu Tyagi <www.vishutyagi8@gmail.com>
2023-09-20 19:48:52 +0530

    Updated Solution - From GFG

--- Basic/Rotate Bits/rotate-bits.cpp
+++ Basic/Rotate Bits/rotate-bits.cpp
@@ -9,7 +9,7 @@ class Solution
         vector <int> rotate (int n, int d)
         {
             //code here.
-            //👀©❌
+            //POTD 🥱
             int a,b;
             d=d%16;
             a=(n<<d|(n>>(16-d)))&0xFFFF;
