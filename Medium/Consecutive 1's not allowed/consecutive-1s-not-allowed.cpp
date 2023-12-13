897b7dbaddb50577d058bc656de5ad5c3cafc5c8
Vishu Tyagi <www.vishutyagi8@gmail.com>
2023-12-13 22:23:52 +0530

    Added Solution - From GFG

---
+++ Medium/Consecutive 1's not allowed/consecutive-1s-not-allowed.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	// #define ll long long
+	ll countStrings(int n) {
+	    // code here
+	    // POTD
+	    // ©❌
+	    vector<ll> dp(n+1,0);
+    dp[0]=1;
+    dp[1]=2;
+    for(int i=2;i<=n;i++){
+        dp[i]=(dp[i-1]+dp[i-2])%1000000007;
+    }
+    return dp[n];
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        auto ans = ob.countStrings(n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
