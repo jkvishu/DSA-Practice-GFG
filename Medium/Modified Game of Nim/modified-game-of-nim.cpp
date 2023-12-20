1a6cb23d216a56e4813f4e323c5becd82dea50d0
Vishu Tyagi <www.vishutyagi8@gmail.com>
2023-12-20 18:56:08 +0530

    Added Solution - From GFG

---
+++ Medium/Modified Game of Nim/modified-game-of-nim.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int findWinner(int n, int A[]){
+        // code here
+        // POTD
+        // 🤝❌😒
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans^=A[i];
+        }
+        return (n&1&&ans>0)?2:1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int A[n];
+        for(int i = 0;i < n;i++)
+            cin>>A[i];
+        
+        Solution ob;
+        cout<<ob.findWinner(n, A)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
