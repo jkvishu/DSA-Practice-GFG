25486d540febbde02f68d24f43a845f0203f2f72
Vishu Tyagi <www.vishutyagi8@gmail.com>
2024-01-04 20:04:50 +0530

    Updated Solution - From GFG

--- Medium/Count Inversions/count-inversions.cpp
+++ Medium/Count Inversions/count-inversions.cpp
@@ -9,55 +9,51 @@ class Solution{
     // arr[]: Input Array
     // N : Size of the Array arr[]
     // Function to count inversions in the array.
-    long long merge(long long int arr[],long long int l,long long int m,long long int r){
-         //❌🤝🤝👀GFG-C
-         long long int n1=m-l+1;
-         long long int n2=r-m;
-         long long int ax1[n1];
-         long long int ax2[n2];
-         for(int i=0;i<n1;i++){
-             ax1[i]=arr[l+i];
-         }
-         for(int j=0;j<n2;j++){
-             ax2[j]=arr[j+m+1];
-         }
-         long long int i=0,j=0,k=l,c=0;
-         while(i<n1&&j<n2){
-             if(ax1[i]<=ax2[j]){
-                 arr[k]=ax1[i];
-                 i++;
-             }
-             else{
-                 c+=(n1-i);
-                 arr[k]=ax2[j];
-                 j++;
-             }
-             k++;
-         }
-         while(i<n1){
-             arr[k++]=ax1[i];
-             i++;
-         }
-         while(j<n2){
-             arr[k++]=ax2[j];
-             j++;
-         }
-         return c;
+    void merge(long long int a[],long long int l,long long int m,long long int h,long long int &ans){
+    long long int n1=m-l+1;
+    long long int n2=h-m;
+    long long int a1[n1],a2[n2];
+    for(int i=0;i<n1;i++){
+        a1[i]=a[l+i];
     }
-    long long find(long long arr[],long long l,long long h){
-        long long res=0;
-        if(l<h){
-        long long q=l+(h-l)/2;
-        res+=find(arr,l,q);
-        res+=find(arr,q+1,h);
-        res+=merge(arr,l,q,h);
+    for(int i=0;i<n2;i++){
+        a2[i]=a[m+i+1];
+    }
+    int i=0,j=0,k=l;
+    while(i<n1&&j<n2){
+        if(a1[i]<=a2[j]){
+            a[k++]=a1[i];
+            i++;
         }
-        return res;
+        else {
+            a[k++]=a2[j];
+            j++;
+            ans+=(n1-i);
+        }
     }
+    while(i<n1){
+        a[k++]=a1[i++];
+    }
+    while(j<n2){
+        a[k++]=a2[j++];
+    }
+}
+
+    void find(long long int a[],long long int l,long long int h,long long int &ans){
+    if(l<h){
+        int m=(l+h)/2;
+        find(a,l,m,ans);
+        find(a,m+1,h,ans);
+        merge(a,l,m,h,ans);
+    }
+}
     long long int inversionCount(long long arr[], long long N)
     {
         // Your Code Here
-        return find(arr,0,N-1);
+        // ✅😏💯
+        long long int ans=0;
+        find(arr,0,N-1,ans);
+        return ans;
     }
 
 };
