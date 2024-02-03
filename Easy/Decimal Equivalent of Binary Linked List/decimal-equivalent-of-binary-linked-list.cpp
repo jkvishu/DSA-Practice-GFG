9641cb5957166d6bbb13def0877eeb06b1a90704
Vishu Tyagi <www.vishutyagi8@gmail.com>
2024-02-03 12:28:01 +0530

    Added Solution - From GFG

---
+++ Easy/Decimal Equivalent of Binary Linked List/decimal-equivalent-of-binary-linked-list.cpp
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+// Program to check if linked list is empty or not
+#include<iostream>
+using namespace std;
+const long long unsigned int MOD = 1000000007;
+
+/* Link list Node */
+struct Node
+{
+    bool data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+
+// } Driver Code Ends
+/* Below global variable is declared in code for modulo arithmetic
+const long long unsigned int MOD = 1000000007; */
+
+/* Link list Node/
+struct Node
+{
+    bool data;   // NOTE data is bool
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+}; */
+
+class Solution
+{
+    public:
+        // Should return decimal equivalent modulo 1000000007 of binary linked list 
+        void find(Node* head,long long unsigned int m,long long unsigned int &ans,long long unsigned int &k){
+            if(head==NULL)
+            return;
+            find(head->next,m,ans,k);
+            ans+=((k*head->data)%m);
+            k=(k*2)%m;
+        }
+        long long unsigned int decimalValue(Node *head)
+        {
+           // Your Code Here
+           // POTD
+           // ✅😏💯
+           if(head==NULL)
+           return 0;
+           long long unsigned int m=1e9+7;
+           long long unsigned int ans=0;
+           long long unsigned int k=1;
+           find(head,m,ans,k);
+           return ans%m;
+        }
+};
+
+
+
+
+//{ Driver Code Starts.
+
+void append(struct Node** head_ref, struct Node **tail_ref, bool new_data)
+{
+
+    struct Node* new_node = new Node(new_data);
+    
+    if (*head_ref == NULL)
+       *head_ref = new_node;
+    else
+       (*tail_ref)->next = new_node;
+    *tail_ref = new_node;
+}
+
+
+bool isEmpty(struct Node *head);
+
+/* Driver program to test above function*/
+int main()
+{
+    bool l;
+    int i, n, T;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n;
+        for(i=1;i<=n;i++)
+        {
+            cin>>l;
+            append(&head, &tail, l);
+        }
+        Solution obj;
+        cout << obj.decimalValue(head) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
