005e7b18a21bf75f264f3f29e5068a5dea230229
Vishu Tyagi <www.vishutyagi8@gmail.com>
2024-02-13 20:05:23 +0530

    Added Solution - From GFG

---
+++ Basic/Pattern Jumping/pattern-jumping.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    string canJump(long long N) {
+        // code here
+        // ✅💯😏
+        if((N&(N-1)))
+        return "False";
+        else
+        return "True";
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.canJump(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
