14ae20cfcc7ccecd2edb458d1e6bd261fb558a7d
Vishu Tyagi <www.vishutyagi8@gmail.com>
2024-02-17 18:37:00 +0530

    Updated Solution - From GFG

--- Medium/Unique Paths in a Grid/unique-paths-in-a-grid.cpp
+++ Medium/Unique Paths in a Grid/unique-paths-in-a-grid.cpp
@@ -30,7 +30,6 @@ class Solution {
         // ✅😏💯
         return find(n-1,m-1,grid)%mod;
     }
-    */
     int find(int n,int m,vector<vector<int>> &grid,vector<vector<int>> &dp){
         if(n==0&&m==0)
         return 1;
@@ -54,6 +53,66 @@ class Solution {
         return 0;
         vector<vector<int>>dp(n,vector<int>(m,-1));
         return find(n-1,m-1,grid,dp)%mod;
+    }
+    int uniquePaths(int n, int m, vector<vector<int>> &grid) {
+        // code here
+        // Optimized approach using Tabulation
+        // ✅😏💯
+        if(grid[0][0]==0||grid[n-1][m-1]==0)
+        return 0;
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        dp[0][0]=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int left=0;
+                int up=0;
+                if(i==0&&j==0)
+                continue;
+                else{
+                    if(j>0&&grid[i][j-1]==1)
+                    left=dp[i][j-1];
+                    if(i>0&&grid[i-1][j]==1)
+                    up=dp[i-1][j];
+                }
+                dp[i][j]=(left+up)%mod;
+            }
+        }
+        return dp[n-1][m-1];
+    }
+    */
+    int uniquePaths(int n, int m, vector<vector<int>> &grid) {
+        // code here
+        // Most Optimized approach consist of space optimization.
+        // ✅😏💯
+        if(grid[0][0]==0||grid[n-1][m-1]==0)
+        return 0;
+        int dp[m];
+        for(int i=0;i<m;i++){
+            dp[i]=0;
+        }
+        for(int i=0;i<n;i++){
+            int temp[m];
+            for(int k=0;k<m;k++){
+                temp[k]=0;
+            }
+            for(int j=0;j<m;j++){
+                if(i==0&&j==0)
+                temp[j]=1;
+                else{
+                    int left=0;
+                    int up=0;
+                    if(j>0&&grid[i][j-1]==1)
+                    left=temp[j-1];
+                    if(i>0&&grid[i-1][j]==1)
+                    up=dp[j];
+                    temp[j]=(left+up)%mod;
+                }
+            }
+            for(int k=0;k<m;k++){
+                dp[k]=temp[k];
+            }
+        }
+        return dp[m-1];
     }
     
 };
