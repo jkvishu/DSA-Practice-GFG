43f7bf4c9f5e0638a6f8fc764c1b6d40e291b0a7
Vishu Tyagi <www.vishutyagi8@gmail.com>
2024-02-22 22:14:09 +0530

    Updated Solution - From GFG

--- Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -9,32 +9,107 @@ using namespace std;
 
 class Solution{   
 public:
+/*
+//1st approach 1st type of recusrive solution i.e mine
+    void find(int i,int n,int cal,int &f,vector<int>&arr,int sum){
+        if(i>=n)
+    return;
+    if(cal==sum){
+        f=1;
+        return;
+    }
+    for (int j = i; j < n; j++)
+    {
+        if ((cal + arr[j]) <= sum)
+        {
+            cal += arr[j];
+            find(j + 1, n, cal, f, arr, sum);
+            cal -= arr[j];
+        }
+    }
+    }
     bool isSubsetSum(vector<int>arr, int sum){
         // code here 
-        // DP solution 
-        // 👀✅🤝©
-        // LITTLe correction from the solution of GFG-C.
-        int m=1e9+7;
-        int n=arr.size();
-        int dp[n+1][sum+1];
-        for(int i=0;i<=n;i++){
-            dp[i][0]=1;
-        }
-        for(int j=1;j<=sum;j++){
-            dp[0][j]=0;
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<=sum;j++){
-                if(j<arr[i-1])
-                dp[i][j]=dp[i-1][j]%m;
-                else
-                dp[i][j]=(dp[i-1][j]%m+dp[i-1][j-arr[i-1]]%m)%m;
-            }
-        }
-        if(dp[n][sum]>0)
+        // Brute force approach
+        // Mine own recursive solution for the problem.
+        // ✅😏💯
+        // TLE
+        if(arr[arr.size()-1]==sum)
         return 1;
+        int f=0;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+        find(i+1,n,arr[i],f,arr,sum);
+        if(f==1)
+        return 1;
+        f=0;
+        }
         return 0;
     }
+    
+//1st appproach 2nd type of recursive solution
+
+    bool find(int i,int n,int cal,int sum,vector<int>&arr){
+        if(i==n){
+            if(cal==sum)
+            return true;
+            else 
+            return false;
+        }
+        if(cal==sum){
+            return true;
+        }
+        cal+=arr[i];
+        if(find(i+1,n,cal,sum,arr))
+        return true;
+        cal-=arr[i];
+        if(find(i+1,n,cal,sum,arr))
+        return true;
+        return false;
+    }
+    bool isSubsetSum(vector<int>arr, int sum){
+        // Brute force approach
+        // Mine own recursive solution for the problem.
+        // ✅😏💯
+        // TLE
+        int n=arr.size();
+        if(find(0,n,0,sum,arr))
+        return true;
+        else
+        return false;
+    }
+    */
+    bool find(int i,int sum,vector<int> &arr,vector< vector<int> >&dp){
+    if(sum==0){
+        return 1;
+    }
+    if(i==0){
+        if(sum==arr[0])
+        return 1;
+        else
+        return 0;
+    }
+    if(dp[i][sum]!=-1)
+    return dp[i][sum];
+    int ntpick=find(i-1,sum,arr,dp);
+    int pick=0;
+    if(arr[i]<=sum)
+    pick=find(i-1,sum-arr[i],arr,dp);
+    return dp[i][sum]=ntpick||pick;
+}
+bool isSubsetSum(vector<int> arr, int sum)
+{
+    // code here
+    // Memoization solution
+    // Striver baba
+    // ✅😏💯🤝
+    int n=arr.size();
+    vector< vector<int> >dp(n,vector<int>(sum+1,-1));
+    if(find(n-1,sum,arr,dp))
+    return 1;
+    else
+    return 0;
+}
 };
 
 //{ Driver Code Starts.
