ff15c390d07d2094873bb0c4e800d623bceb5482
Vishu Tyagi <www.vishutyagi8@gmail.com>
2024-02-24 09:49:57 +0530

    Updated Solution - From GFG

--- Hard/Minimum sum partition/minimum-sum-partition.cpp
+++ Hard/Minimum sum partition/minimum-sum-partition.cpp
@@ -8,35 +8,37 @@ class Solution{
   public:
 	int minDifference(int arr[], int n)  { 
 	    // Your code goes here
-	    // ❌❌
-	    int sum=0;
-    for(int i=0;i<n;i++){
-        sum+=arr[i];
-    }
-     bool t[n+1][sum+1];
-       for(int i=0;i<sum+1;i++)
-           t[0][i]=false;
-       for(int i=0;i<n+1;i++)
-           t[i][0]=true;
-       
-       for(int i=1;i<n+1;i++){
-           for(int j=1;j<sum+1;j++){
-               if(arr[i-1]<=j){
-                   t[i][j]=t[i-1][j]||t[i-1][j-arr[i-1]];
-               }
-               else{
-                   t[i][j]=t[i-1][j];
-               }
-           }
-           
-       }    
-      int mn =INT_MAX;
-   for(int i=0; i<=sum/2; i++) {
-       if(t[n][i]) {
-           mn = min(mn, sum-(2*i));
-       }
-   }
-   return mn;
+	    // ✅😏💯🤝
+	    // Striver baba
+	    // Solved and Coded in 3 min 😎
+	    long long int sum=0,psum=0;
+	    for(int i=0;i<n;i++){
+	        sum+=arr[i];
+	        psum+=arr[i];
+	    }
+	    vector< vector<int> >dp(n,vector<int>(sum+1,0));
+	    for(int i=0;i<n;i++){
+	        dp[i][0]=1;
+	    }
+	    if(arr[0]<=sum)
+	    dp[0][arr[0]]=1;
+	    for(int i=1;i<n;i++){
+	        for(int j=1;j<=sum;j++){
+	            int pick=0;
+	            if(arr[i]<=j)
+	            pick=dp[i-1][j-arr[i]];
+	            int ntpick=dp[i-1][j];
+	            dp[i][j]=pick|ntpick;
+	        }
+	    }
+	    int ans=1e9;
+	    for(int i=0;i<=sum/2;i++){
+	        if(dp[n-1][i]){
+	            int s1=psum-i;
+	            ans=min(ans,abs(s1-i));
+	        }
+	    }
+	    return ans;
 	} 
 };
 
