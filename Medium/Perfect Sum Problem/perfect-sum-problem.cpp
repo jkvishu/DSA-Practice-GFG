96668c77370ee809b829c22c8d2203d07d3e1030
Vishu Tyagi <www.vishutyagi8@gmail.com>
2024-02-24 21:09:29 +0530

    Updated Solution - From GFG

--- Medium/Perfect Sum Problem/perfect-sum-problem.cpp
+++ Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -3,34 +3,59 @@
 using namespace std;
 
 // } Driver Code Ends
-class Solution{
 
+int mod=1e9+7;
+class Solution{
 	public:
+	/*
+	int find(int arr[],int n,int sum){
+	    if(n==0){
+	        if(arr[0] == 0 && sum == 0) return 2;
+            if(arr[0] == sum || sum == 0) return 1;
+            return 0;
+	    }
+	    int ntpick=find(arr,n-1,sum);
+	    int pick=0;
+	    if(arr[n]<=sum)
+	    pick=find(arr,n-1,sum-arr[n]);
+	    return (ntpick+pick);
+	}
 	int perfectSum(int arr[], int n, int sum)
 	{
         // Your code goes here
-         // DP solution 
-        // 👀✅🤝©
-        // LITTLe correction from the solution of GFG-C.
-        int m=1e9+7;
-        int dp[n+1][sum+1];
-        for(int i=0;i<=n;i++){
-            dp[i][0]=1;
-        }
-        for(int j=1;j<=sum;j++){
-            dp[0][j]=0;
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<=sum;j++){
-                if(j<arr[i-1])
-                dp[i][j]=dp[i-1][j]%m;
-                else
-                dp[i][j]=(dp[i-1][j]%m+dp[i-1][j-arr[i-1]]%m)%m;
-            }
-        }
-       return dp[n][sum]%m;
-	}
-	  
+        // brute force approach
+        // Recursive solution for the problem.
+        // ✅😏💯
+        // TLE
+       return find(arr,n-1,sum)%mod;
+	}*/
+// 	int mod=1e9+7;
+int find(int arr[], int n, int sum,vector< vector<int> >&dp)
+{
+    if (n == 0)
+    {
+        if (arr[0] == 0 && sum == 0)
+            return 2;
+        if (arr[0] == sum || sum == 0)
+            return 1;
+        return 0;
+    }
+    if(dp[n][sum]!=-1)
+    return dp[n][sum];
+    int ntpick = find(arr, n - 1, sum,dp);
+    int pick = 0;
+    if (arr[n] <= sum)
+        pick = find(arr, n - 1, sum - arr[n],dp);
+    return dp[n][sum]=(ntpick + pick) % mod;
+}
+int perfectSum(int arr[], int n, int sum)
+{
+    // Your code goes here
+    // Memoization solution for the problem.
+    // ✅😏💯
+    vector< vector<int> >dp(n,vector<int>(sum+1,-1));
+    return find(arr, n - 1, sum,dp) % mod;
+}
 };
 
 //{ Driver Code Starts.
