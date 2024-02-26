f8041467dc0ae2d3d8dfd5116cb992eedf8bddf4
Vishu Tyagi <www.vishutyagi8@gmail.com>
2024-02-26 20:38:26 +0530

    Updated Solution - From GFG

--- Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
+++ Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int mod=1e9+7;
 class Solution {
   public:
+  /*
     int countPartitions(int n, int d, vector<int>& arr) {
         // Code here
         // Direct approach Striver baba
@@ -36,6 +37,39 @@ class Solution {
         }
         return dp[n-1][sum];
     }
+    */
+    int countPartitions(int n, int d, vector<int>& arr) {
+        // Code here
+        // Direct optimal approach Striver baba
+        // ✅💯😏🤝
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+        }
+        if(sum-d<0||(sum-d)%2!=0)
+        return 0;
+        sum=(sum-d)/2;
+    vector<int>dp(sum+1,0);
+    vector<int>cur(sum+1,0);
+     if(arr[0]==0) 
+        dp[0]=2;
+        else 
+        dp[0]=1;
+        if(arr[0]!=0 && arr[0] <= sum) 
+        dp[arr[0]]=1;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=sum;j++){
+                int pick=0;
+                if(arr[i]<=j)
+                pick=dp[j-arr[i]];
+                int ntpick=dp[j];
+                cur[j]=(pick+ntpick)%mod;
+            }
+            dp=cur;
+        }
+    return dp[sum];
+    }
+    
 };
 
 //{ Driver Code Starts.
