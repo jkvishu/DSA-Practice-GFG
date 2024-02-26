e738594f128ba1e08e10b459ff935105dda209d5
Vishu Tyagi <www.vishutyagi8@gmail.com>
2024-02-26 19:33:11 +0530

    Update power-set.cpp

--- Medium/Power Set/power-set.cpp
+++ Medium/Power Set/power-set.cpp
@@ -5,6 +5,49 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
 	public:
+	/*
+	private:
+    static bool cmp(string a,string b){
+	        if(a[0]==b[0]){
+	            int i=0;
+	            while(a[i]==b[i])
+	            {
+	                i++;
+	                if(i<a.size()&&i<b.size()){
+	                    continue;
+	                }
+	                else{
+	                    return a.size()<b.size();
+	                }
+	            }
+	            return a[i]<b[i];
+	        }
+	        else{
+	            return a[0]<b[0];
+	        }
+	    }
+	public:
+	    vector<string> AllPossibleStrings(string s)
+		{
+		    // Code here
+		    //Done on my own with my own logic + below algo is learn from GFG
+		    int len=s.length();
+		    vector<string>v;
+		    string st="";
+		    long long int p=pow(2,len);
+		    for(int i=1;i<p;i++){
+		        for(int j=0;j<len;j++){
+		            if((i&(1<<j))!=0){
+		                st.push_back(s[j]);
+		            }
+		        }
+		        v.push_back(st);
+		        st="";
+		    }
+		    sort(v.begin(),v.end(),cmp);
+		    return v;
+		}
+	*/
 	    void find(int i,vector<string>&ans,string st,string s,int n){
 	        if(i==n){
 	            if(st.length()>0)
