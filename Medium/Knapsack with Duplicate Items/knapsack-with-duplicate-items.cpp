8e5b5398f231656d4a7fd682be56e7d9279bf18c
Vishu Tyagi <www.vishutyagi8@gmail.com>
2024-03-04 00:09:18 +0530

    Updated Solution - From GFG

--- Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
+++ Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
@@ -9,25 +9,73 @@ using namespace std;
 
 class Solution{
 public:
-    int knapSack(int N, int W, int val[], int wt[])
-    {
-        // code here
-        // POTD
-        vector<vector<int>> arr(N, vector<int>(W + 1, 0));
+/*
+    int find(int n,int w,int val[],int wt[]){
+        if(n==0){
+            if(wt[0]<=w)
+            return (w/wt[0])*val[0];
+            else 
+            return 0;
+        }
+        int pick=INT_MIN;
+        if(wt[n]<=w)
+        pick=val[n]+find(n,w-wt[n],val,wt);
+        int ntpick=0+find(n-1,w,val,wt);
+        return max(pick,ntpick);
         
-        for(int i=0;i<N;i++){
-            for(int j=1;j<=W;j++) {
-                int pick = 0, notPick = 0;
-                if(wt[i]<=j) {
-                    pick  = val[i] + arr[i][j-wt[i]];
-                }
-                if(i>0) notPick = arr[i-1][j];
-
-                arr[i][j] = max(pick,notPick);
+    }
+    int knapSack(int n, int W, int val[], int wt[])
+    {
+        //Code here
+        // brute force approach
+        // Recursive solution for the problem.
+        // ✅😏💯
+        // TLE
+        return find(n-1,W,val,wt);
+    }
+    */
+    int find(int n,int w,int val[],int wt[],vector<vector<int>>&dp){
+        if(n==0){
+            if(wt[0]<=w)
+            return (w/wt[0])*val[0];
+            else 
+            return 0;
+        }
+        if(dp[n][w]!=-1)
+        return dp[n][w];
+        int pick=INT_MIN;
+        if(wt[n]<=w)
+        pick=val[n]+find(n,w-wt[n],val,wt,dp);
+        int ntpick=0+find(n-1,w,val,wt,dp);
+        return dp[n][w]=max(pick,ntpick);
+        
+    }
+    int knapSack(int n, int W, int val[], int wt[])
+    {
+        // Code here
+        // Memoization solution for the problem.
+        // ✅😏💯
+        vector<vector<int>>dp(n,vector<int>(W+1,-1));
+        return find(n-1,W,val,wt,dp);
+    }
+    
+    /*{
+        // code here
+        vector<int>dp(W+1,0);
+        for(int i=wt[0];i<=W;i++){
+            dp[i]=val[0];
+        }
+        for(int i=1;i<n;i++){
+            for(int j=W;j>=0;j--){
+                int pick=INT_MIN;
+                if(wt[i]<=j)
+                pick=val[i]+dp[j-wt[i]];
+                int ntpick=0+dp[j];
+                dp[j]=max(pick,ntpick);
             }
         }
-        return arr[N-1][W];
-    }
+        return dp[W];
+    }*/
 };
 
 //{ Driver Code Starts.
