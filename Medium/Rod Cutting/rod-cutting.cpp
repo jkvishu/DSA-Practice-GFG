44301ebe837af9a50b8466ca3b1ac173554b29f7
Vishu Tyagi <www.vishutyagi8@gmail.com>
2024-03-04 19:24:07 +0530

    Updated Solution - From GFG

--- Medium/Rod Cutting/rod-cutting.cpp
+++ Medium/Rod Cutting/rod-cutting.cpp
@@ -10,23 +10,43 @@ using namespace std;
 
 class Solution{
   public:
+  /*
+    int find(int price[],int n,int w){
+        if(n==0){
+            return price[0]*w;
+        }
+        int pick=INT_MIN;
+        if((n+1)<=w)
+        pick=price[n]+find(price,n,w-(n+1));
+        int ntpick=0+find(price,n-1,w);
+        return max(pick,ntpick);
+    }
     int cutRod(int price[], int n) {
         //code here
-        //POTD ❌©👀
-        int arr[n];
-        arr[0]=0;
-        int i,ans,j;
-        for(i=1;i<=n;i++)
-        {
-            ans=INT_MIN;
-            for(j=0;j<i;j++)
-            {
-                ans=max(ans,price[j]+arr[i-j-1]);
-            }
-            arr[i]=ans;
-        }
-        return arr[n];
+        
+        return find(price,n-1,n);
     }
+    */
+    int find(int price[],int n,int w,vector<vector<int>>&dp){
+        if(n==0){
+            return price[0]*w;
+        }
+        if(dp[n][w]!=-1)
+        return dp[n][w];
+        int pick=INT_MIN;
+        if((n+1)<=w)
+        pick=price[n]+find(price,n,w-(n+1),dp);
+        int ntpick=0+find(price,n-1,w,dp);
+        return dp[n][w]=max(pick,ntpick);
+    }
+    int cutRod(int price[], int n) {
+        //code here
+        // Memoization solution for the problem.
+        // ✅😏💯
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        return find(price,n-1,n,dp);
+    }
+    
 };
 
 //{ Driver Code Starts.
