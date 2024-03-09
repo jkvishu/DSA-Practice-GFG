665712e0d5bafe863a9440a01d822bb404af2ddc
Vishu Tyagi <www.vishutyagi8@gmail.com>
2024-03-09 07:34:42 +0530

    Updated Solution - From GFG

--- Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.cpp
+++ Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.cpp
@@ -10,32 +10,60 @@ using namespace std;
 
 class Solution{
   public:
-    int longestPalinSubseq(string a) {
-        //code here
-        string b;
-    
-        int n=a.length();
-        for(int i=n-1;i>=0;i--){
-            b.push_back(a[i]);
+  /*
+    int find(string s1,string s2,int n,int m){
+        if(n<0||m<0)
+        return 0;
+        int pick=0;
+        int ntpick=0;
+        if(s1[n]==s2[m]){
+            pick=1+find(s1,s2,n-1,m-1);
         }
-        int c[n+1][n+1];
-        for(int i=0;i<=n;i++)
-        c[i][0]=0;
-         for(int i=0;i<=n;i++)
-        c[0][i]=0;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(a[i-1]==b[j-1])
-                    c[i][j]=c[i-1][j-1]+1;
-                    else if(c[i-1][j]>=c[i][j-1])
-                    c[i][j]=c[i-1][j];
-                    else
-                    c[i][j]=c[i][j-1];
-                }
-            }
-            
-             return c[n][n];
-        
+        else{
+            int t1=find(s1,s2,n-1,m);
+            int t2=find(s1,s2,n,m-1);
+            ntpick=max(t1,t2);
+        }
+        return max(pick,ntpick);
+    }
+    int longestPalinSubseq(string A) {
+        //code here
+        //  Brute force approach
+        //  Mine own recursive solution for the problem.
+        //  ✅😏💯
+        //  TLE
+        string b=A;
+        reverse(b.begin(),b.end());
+        int n=A.length();
+        return find(A,b,n-1,n-1);
+    }
+    */
+    int find(string s1,string s2,int n,int m,vector<vector<int>>&dp){
+        if(n<0||m<0)
+        return 0;
+        int pick=0;
+        int ntpick=0;
+        if(dp[n][m]!=-1)
+        return dp[n][m];
+        if(s1[n]==s2[m]){
+            pick=1+find(s1,s2,n-1,m-1,dp);
+        }
+        else{
+            int t1=find(s1,s2,n-1,m,dp);
+            int t2=find(s1,s2,n,m-1,dp);
+            ntpick=max(t1,t2);
+        }
+        return dp[n][m]=max(pick,ntpick);
+    }
+    int longestPalinSubseq(string A) {
+        //code here
+        // Memoization solution for the problem.
+        // ✅😏💯
+        string b=A;
+        reverse(b.begin(),b.end());
+        int n=A.length();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        return find(A,b,n-1,n-1,dp);
     }
 };
 
