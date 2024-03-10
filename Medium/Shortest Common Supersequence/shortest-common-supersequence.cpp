ab7b7b880ec5a1a5aae712efb8f63ab05796fe0c
Vishu Tyagi <www.vishutyagi8@gmail.com>
2024-03-10 08:32:36 +0530

    Updated Solution - From GFG

--- Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
+++ Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
@@ -13,30 +13,25 @@ class Solution
 {
     public:
     //Function to find length of shortest common supersequence of two strings.
-    int shortestCommonSupersequence(string X, string Y, int m, int n)
+    int shortestCommonSupersequence(string s1, string s2, int n, int m)
     {
         //code here
-        //❌©👀
-        int dp[m+1][n+1];
-        for(int i=0;i<=m;i++)
+        // Space Optimized solution.
+        // ✅😏💯
+        vector<int> dp(m + 1, 0);
+        vector<int> cur(m + 1, 0);
+        for (int i = 1; i <= n; i++)
         {
-            for(int j=0;j<=n;j++)
-            {
-                if(i==0 || j==0)
-                {
-                    dp[i][j]=0;
-                }
-                else if(X[i-1]==Y[j-1])
-                {
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else
-                {
-                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
-                }
-            }
+        for (int j = 1; j <= m; j++)
+        {
+            if (s1[i - 1] == s2[j - 1])
+                cur[j] = 1 + dp[j - 1];
+            else
+                cur[j] = max(dp[j], cur[j - 1]);
         }
-        return m+n-dp[m][n];
+        dp = cur;
+        }
+        return ((n+m)-dp[m]);
     }
 };
 
