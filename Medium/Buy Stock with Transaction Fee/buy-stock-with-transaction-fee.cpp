6c0f52630e662cdd1ffb6986bc04187875e40fdc
Vishu Tyagi <www.vishutyagi8@gmail.com>
2024-03-17 21:07:03 +0530

    Updated Solution - From GFG

--- Medium/Buy Stock with Transaction Fee/buy-stock-with-transaction-fee.cpp
+++ Medium/Buy Stock with Transaction Fee/buy-stock-with-transaction-fee.cpp
@@ -5,18 +5,54 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
+    /*
+    long long int find(int i, int n, vector<long long int> &prices, bool f, int pro,int fee)
+    {
+    if (i == n)
+        return 0;
+    if (f)
+    {
+        pro = max(-prices[i] + find(i + 1, n, prices, 0, pro,fee)-fee, 0 + find(i + 1, n, prices, 1, pro,fee));
+    }
+    else
+    {
+        pro = max(prices[i] + find(i + 1, n, prices, 1, pro,fee), 0 + find(i + 1, n, prices, 0, pro,fee));
+    }
+    return pro;
+    }
     long long maximumProfit(vector<long long>&prices, int n, int fee) {
         // Code here
-        //This question is a little variation of Cool down Stock buy and sell problem
-        //So it was easy for me to solve it in O(1) Space 
-        //Cpypcpcpt
-        long long s0=INT_MIN,s1=0; 
-        for(long long p:prices) {
-            long long int tmp1=s0;
-            s0=max(s0,s1-p);
-            s1=max(s1,tmp1+p-fee);
-        }
-        return s1;
+        // Brute force approach
+        // TLE
+        // Recusrive solution
+        // ✅💯😏
+        return find(0, n, prices, 1, 0,fee);
+    }
+    */
+      long long int find(int i, int n, vector<long long int> &prices, bool f, int pro,int fee,vector<vector<long long int>> &dp)
+    {
+    if (i == n)
+        return 0;
+    if(dp[i][f]!=-1)
+    return dp[i][f];
+    if (f)
+    {
+        pro = max(-prices[i] + find(i + 1, n, prices, 0, pro,fee,dp)-fee, 0 + find(i + 1, n, prices, 1, pro,fee,dp));
+    }
+    else
+    {
+        pro = max(prices[i] + find(i + 1, n, prices, 1, pro,fee,dp), 0 + find(i + 1, n, prices, 0, pro,fee,dp));
+    }
+    return dp[i][f]=pro;
+    }
+    long long maximumProfit(vector<long long>&prices, int n, int fee) {
+        // Code here
+        // Brute force approach
+        // TLE
+        // Recusrive solution
+        // ✅💯😏
+        vector<vector<long long int>> dp(n + 1, vector<long long int>(2, -1));
+        return find(0, n, prices, 1, 0,fee,dp);
     }
 };
 
