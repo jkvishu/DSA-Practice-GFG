c57874d69f4535bd02e302bca6d5fdd2809d3852
Vishu Tyagi <www.vishutyagi8@gmail.com>
2024-03-19 20:39:54 +0530

    Updated Solution - From GFG

--- Medium/Longest Bitonic subsequence/longest-bitonic-subsequence.cpp
+++ Medium/Longest Bitonic subsequence/longest-bitonic-subsequence.cpp
@@ -2,56 +2,87 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// } Driver Code Ends
-class Solution{
-	public:
-	int LongestBitonicSequence(vector<int>nums)
-	{
-	    // code here
-	    // ✅💯🤝👀😏
-	    int n=nums.size();
-	    int dp1[n]={0};
-	    int dp2[n]={0};
-	    dp1[0]=1;
-	    for(int i=1;i<n;i++){
-	        dp1[i]=1;
-	        for(int j=0;j<i;j++){
-	            if(nums[j]<nums[i]){
-	                dp1[i]=max(dp1[i],dp1[j]+1);
-	            }
-	        }
-	    }
-	    dp2[n-1]=1;
-	    for(int i=n-2;i>=0;i--){
-	        dp2[i]=1;
-	        for(int j=n-1;j>i;j--){
-	            if(nums[j]<nums[i]){
-	                dp2[i]=max(dp2[i],dp2[j]+1);
-	            }
-	        }
-	    }
-	    int ans=INT_MIN;
-	    for(int i=0;i<n;i++){
-	        ans=max(ans,dp1[i]+dp2[i]-1);
-	    }
-	    return ans;
-	}
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
 };
 
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int LongestBitonicSequence(int n, vector<int> &nums) {
+        // code here
+        // Direct optimal solution.
+        // Most Optimal Solution
+        // ✅😏💯
+	   // Direct optimal solution.
+        // Most Optimal Solution
+        // ✅😏💯
+        vector<int>dp1(n,1);
+        vector<int>dp2(n,1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]&&dp1[i]<dp1[j]+1)
+                dp1[i]=1+dp1[j];
+            }
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int j=n-1;j>i;j--){
+                if(nums[j]<nums[i]&&dp2[i]<dp2[j]+1)
+                dp2[i]=1+dp2[j];
+            }
+        }
+        int ans=0;
+        for(int i=1;i<n-1;i++){
+            if(dp1[i]>1&&dp2[i]>1)
+            ans=max(ans,dp1[i]+dp2[i]-1);
+        }
+        return ans;
+    }
+};
+
+
 //{ Driver Code Starts.
+
 int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int n;
-		cin >> n;
-		vector<int>nums(n);
-		for(int i = 0; i < n; i++)
-			cin >> nums[i];
-		Solution ob;
-		int ans = ob.LongestBitonicSequence(nums);
-		cout << ans <<"\n";
-	}
-	return 0;
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int n;
+        scanf("%d",&n);
+        
+        
+        vector<int> nums(n);
+        Array::input(nums,n);
+        
+        Solution obj;
+        int res = obj.LongestBitonicSequence(n, nums);
+        
+        cout<<res<<endl;
+        
+    }
 }
+
 // } Driver Code Ends
