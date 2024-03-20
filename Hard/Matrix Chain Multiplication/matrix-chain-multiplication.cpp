ff760f80db93efc1cf89e30f405986db950578f1
Vishu Tyagi <www.vishutyagi8@gmail.com>
2024-03-20 23:16:44 +0530

    Updated Solution - From GFG

--- Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
+++ Hard/Matrix Chain Multiplication/matrix-chain-multiplication.cpp
@@ -9,25 +9,46 @@ using namespace std;
 
 class Solution{
 public:
+/*
+    int find(int i,int j,int a[]){
+        if(i==j)
+        return 0;
+        int ans=1e8;
+        for(int k=i;k<j;k++){
+            int steps=a[i-1]*a[k]*a[j]+find(i,k,a)+find(k+1,j,a);
+            ans=min(ans,steps);
+        }
+        return ans;
+    }
     int matrixMultiplication(int N, int arr[])
     {
         // code here
-        // DP one of the tricky and best question
-        // ✅🤝©👀
-        int dp[N][N];
-        for(int i=0;i<N-1;i++){
-            dp[i][i+1]=0;
+        // TLE
+        // Recursive solution
+        // ✅😏💯🤝
+        return find(1,N-1,arr);
+    }
+    */
+    int find(int i,int j,int a[],vector<vector<long long int>>&dp){
+        if(i==j)
+        return 0;
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+        int ans=INT_MAX;
+        for(int k=i;k<j;k++){
+            int steps=a[i-1]*a[k]*a[j]+find(i,k,a,dp)+find(k+1,j,a,dp);
+            ans=min(ans,steps);
         }
-        for(int gap=2;gap<N;gap++){
-            for(int i=0;i+gap<N;i++){
-                int j=i+gap;
-                dp[i][j]=INT_MAX;
-                for(int k=i+1;k<j;k++){
-                    dp[i][j]=min(dp[i][j],dp[i][k]+dp[k][j]+arr[i]*arr[j]*arr[k]);
-                }
-            }
-        }
-        return dp[0][N-1];
+        return dp[i][j]=ans;
+    }
+    int matrixMultiplication(int N, int arr[])
+    {
+        // code here
+        // Memoization solution for the problem.
+        // ✅😏💯🤝
+        // Still TLE
+        vector<vector<long long int>>dp(N,vector<long long int>(N,-1));
+        return find(1,N-1,arr,dp);
     }
 };
 
