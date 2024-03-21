fa71e34b8a52337ab416da332817fc9c3602f929
Vishu Tyagi <www.vishutyagi8@gmail.com>
2024-03-21 17:32:45 +0530

    Updated Solution - From GFG

--- Hard/Boolean Parenthesization/boolean-parenthesization.cpp
+++ Hard/Boolean Parenthesization/boolean-parenthesization.cpp
@@ -9,76 +9,95 @@ using namespace std;
 
 class Solution{
 public:
-    int mod= 1003;
-
-    int solve(int i, int j, int isTrue, string &s, vector<vector<vector<int>>> &dp){
-        if(i>j) return 0;
-        if(dp[i][j][isTrue] != -1) return dp[i][j][isTrue]; 
+/*
+    int find(string &s, int i, int j, bool isTrue){
+        if(i > j) return 0; //If there are no elements then return 0
         
-        if(i==j){
-            if(isTrue){
-                if(s[i]=='T') return 1;
-                else{
-                    return 0;
-                }
+        //Base case, if there is only one element
+        if(i == j){
+            if(isTrue) return s[i] == 'T';
+            else return s[i] == 'F';
+        }
+        
+        int ans = 0;
+        
+        // All possibilities for getting true and false as well
+        for(int k=i+1;k<=j-1;k+=2){
+            int LT = find(s, i, k-1, true);
+            int LF = find(s, i, k-1, false);
+            int RT = find(s, k+1, j, true);
+            int RF = find(s, k+1, j, false);
+            
+            if(s[k] == '&'){
+                if(isTrue) ans += LT*RT; //'AND' operation for getting 'True' as answer
+                else ans += LF*RF + LF*RT + LT*RF; //'AND' operation for getting 'False' as answer
             }
-            else{
-                if(s[i]=='F') return 1;
-                else{
-                    return 0;
-                }
+            
+            else if(s[k] == '|'){
+                if(isTrue) ans += LT*RT + LT*RF + LF*RT; //'OR' operation for getting 'True' as answer
+                else ans += LF*RF; //'OR' operation for getting 'False' as answer
+            }
+            
+            else if(s[k] = '^'){
+                if(isTrue) ans += LT*RF + LF*RT; //'XOR' operation for getting 'True' as answer
+                else ans += LT*RT + LF*RF; //'XOR' operation for getting 'False' as answer
             }
         }
         
-        int ways=0;
+        return ans%1003;
+    }
+    
+    int countWays(int N, string S){
+        // Recursive Solution
+        // ✅😏💯
+        // TLE
+        return find(S, 0, N-1, true);
+    }
+    */
+    int find(string &s, int i, int j, bool isTrue,vector<vector<vector<int>>>&dp){
+        if(i > j) return 0; //If there are no elements then return 0
         
-        for(int index=i+1; index<j; index+=2){
-            int lt= solve(i,index-1,1,s,dp);
-            int lf= solve(i,index-1,0,s,dp);
-            int rt= solve(index+1,j,1,s,dp);
-            int rf= solve(index+1,j,0,s,dp);
-            
-            if(s[index]=='&'){
-                if(isTrue){
-                    ways= (ways%mod + (lt % mod * rt % mod) % mod ) % mod; 
-                }
-                else{
-                    ways= (ways%mod + (lf % mod * rt % mod)%mod + (lt % mod * rf % mod)%mod + (lf % mod * rf % mod)%mod ) % mod;
-                }
-            }
-            
-            else if(s[index]=='|'){
-                if(isTrue){
-                    ways= (ways%mod + (lf % mod * rt % mod)%mod + (lt % mod * rf % mod)%mod + (lt % mod * rt % mod)% mod )%mod;
-                }
-                else{
-                    ways=(ways%mod + (lf % mod * rf % mod)% mod) % mod;
-                }
-            }
-            else{
-                if(isTrue){
-                    ways= (ways%mod + (lf % mod * rt % mod)%mod + (lt % mod * rf % mod)% mod) % mod;
-                }
-                else{
-                    ways=(ways%mod + (lt % mod * rt % mod)%mod + (lf % mod * rf % mod)%mod) % mod;
-                }
-                
-            }
-            
-            
+        //Base case, if there is only one element
+        if(i == j){
+            if(isTrue) return s[i] == 'T';
+            else return s[i] == 'F';
         }
-        dp[i][j][isTrue]= ways % mod;
+        
+        int ans = 0;
+        if(dp[i][j][isTrue]!=-1)
         return dp[i][j][isTrue];
         
+        // All possibilities for getting true and false as well
+        for(int k=i+1;k<=j-1;k+=2){
+            int LT = find(s, i, k-1, true,dp);
+            int LF = find(s, i, k-1, false,dp);
+            int RT = find(s, k+1, j, true,dp);
+            int RF = find(s, k+1, j, false,dp);
+            
+            if(s[k] == '&'){
+                if(isTrue) ans += LT*RT; //'AND' operation for getting 'True' as answer
+                else ans += LF*RF + LF*RT + LT*RF; //'AND' operation for getting 'False' as answer
+            }
+            
+            else if(s[k] == '|'){
+                if(isTrue) ans += LT*RT + LT*RF + LF*RT; //'OR' operation for getting 'True' as answer
+                else ans += LF*RF; //'OR' operation for getting 'False' as answer
+            }
+            
+            else if(s[k] = '^'){
+                if(isTrue) ans += LT*RF + LF*RT; //'XOR' operation for getting 'True' as answer
+                else ans += LT*RT + LF*RF; //'XOR' operation for getting 'False' as answer
+            }
+        }
         
+        return dp[i][j][isTrue]=ans%1003;
     }
-
+    
     int countWays(int N, string S){
-        //code here
-        //POTD 21/02/2024
-        //©❌👀
-        vector<vector<vector<int>>>dp(N+1, vector<vector<int>>(N+1, vector<int>(3,-1)));
-        return solve(0,N-1,1,S,dp);
+        // Memoization solution for the problem.
+        // ✅😏💯
+        vector<vector<vector<int>>>dp(N+1,vector<vector<int>>(N+1,vector<int>(2,-1)));
+        return find(S, 0, N-1, true,dp);
     }
 };
 
