877adf063a6af84162c81176aabf36b48a39e45c
Vishu Tyagi <www.vishutyagi8@gmail.com>
2024-03-29 22:54:59 +0530

    Updated Solution - From GFG

--- Medium/Rotten Oranges/rotten-oranges.cpp
+++ Medium/Rotten Oranges/rotten-oranges.cpp
@@ -9,49 +9,46 @@ class Solution
     //Function to find minimum time required to rot all oranges. 
     int orangesRotting(vector<vector<int>>& grid) {
         // Code here
-        // ✅🤝💯👀
+        // ✅😏💯
         // Striver OP
+        queue<pair<int,pair<int,int>>>pq;
+        int dlrow[]={-1,0,+1,0};
+        int dlcol[]={0,+1,0,-1};
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        queue<pair<pair<int,int>,int>>pq;
-        int fcount=0;
+        int ck[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                pq.push({{i,j},0});
-                vis[i][j]=2;
+                pq.push({0,{i,j}});
+                ck[i][j]=2;
                 }
                 else{
-                    vis[i][j]=0;
+                    ck[i][j]=0;
                 }
-                if(grid[i][j]==1)
-                fcount++;
             }
         }
-        int mxt=0;
-        int c=0;
+        int ans=0;
         while(!pq.empty()){
-            int row=pq.front().first.first;
-            int col=pq.front().first.second;
-            int t=pq.front().second;
-            mxt=max(mxt,t);
+            auto it=pq.front();
             pq.pop();
-            int rown[]={-1,0,+1,0};
-            int coln[]={0,+1,0,-1};
-            for(int i=0;i<4;i++){
-                int nrow=row+rown[i];
-                int ncol=col+coln[i];
-                if(nrow<n&&nrow>=0&&ncol<m&&ncol>=0&&grid[nrow][ncol]==1&&vis[nrow][ncol]==0){
-                    pq.push({{nrow,ncol},t+1});
-                    vis[nrow][ncol]=2;
-                    c++;
-                }
+            ans=max(ans,it.first);
+            for(int i=0;i<=3;i++){
+                    int nr=dlrow[i]+it.second.first;
+                    int nc=dlcol[i]+it.second.second;
+                    if(nr>=0&&nr<n&&nc>=0&&nc<m&&grid[nr][nc]==1&&ck[nr][nc]==0){
+                        ck[nr][nc]=2;
+                        pq.push({it.first+1,{nr,nc}});
+                    }
             }
         }
-        if(c!=fcount)
-        return -1;
-        return mxt;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(ck[i][j]==0&&grid[i][j]==1)
+                return -1;
+            }
+        }
+        return ans;
     }
 };
 
