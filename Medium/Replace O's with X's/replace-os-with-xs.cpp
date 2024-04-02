65b07994680342496d713fed74d6328e5e87a7f0
Vishu Tyagi <www.vishutyagi8@gmail.com>
2024-04-02 20:25:41 +0530

    Updated Solution - From GFG

--- Medium/Replace O's with X's/replace-os-with-xs.cpp
+++ Medium/Replace O's with X's/replace-os-with-xs.cpp
@@ -8,36 +8,54 @@ using namespace std;
 // User function Template for C++
 
 class Solution{
-private:
-    int delta[5] = {-1,0,+1,0,-1};
-    void dfs(int row,int col,vector<vector<char>> &mat){
-        int n = mat.size() , m = mat[0].size();
-        mat[row][col] = '#';
-        for(int d=0;d<4;d++){
-            int nRow = row + delta[d];
-            int nCol = col + delta[d+1];
-            if(min(nRow,nCol) >= 0 && nRow < n && nCol < m && mat[nRow][nCol] == 'O')
-                dfs(nRow,nCol,mat);
+public:
+    void dfs(vector<vector<char>> &mat,vector<vector<bool>>&vis,int i,int j){
+        vis[i][j]=1;
+        int row[] = {-1, 0, +1, 0};
+        int col[] = {0, +1, 0, -1};
+        int n=mat.size();
+        int m=mat[0].size();
+        for(int k=0;k<4;k++){
+            int nr=row[k]+i;
+            int nc=col[k]+j;
+            if(nr>=0&&nr<n&&nc>=0&&nc<m&&mat[nr][nc]=='O'&&vis[nr][nc]==false){
+            vis[nr][nc]=true;
+            dfs(mat,vis,nr,nc);
+            }
         }
     }
-public:
-    vector<vector<char>> fill(int n, int m, vector<vector<char>> mat){
-        //👀©❌
-        //POTD
-        for(int row=0;row<n;row++){
-            for(int col : {0,m-1}){
-                if(mat[row][col] == 'O') dfs(row,col,mat);
+    vector<vector<char>> fill(int n, int m, vector<vector<char>> mat)
+    {
+        // code here
+        // ✅😏💯🤝
+        // Striver OP
+        vector<vector<bool>>vis(n,vector<bool>(m,false));
+        for(int i=0;i<n;i++){
+            if(mat[i][0]=='O'&&vis[i][0]==0){
+                dfs(mat,vis,i,0);
             }
         }
-        for(int col=0;col<m;col++){
-            for(int row : {0,n-1}){
-                if(mat[row][col] == 'O') dfs(row,col,mat);
+        for(int i=0;i<m;i++){
+            if(mat[n-1][i]=='O'&&vis[n-1][i]==0){
+                dfs(mat,vis,n-1,i);
             }
         }
-        for(int row=0;row<n;row++)
-            for(int col=0;col<m;col++) 
-                if(mat[row][col] == 'O') mat[row][col] = 'X';
-                else if(mat[row][col] == '#') mat[row][col] = 'O';
+        for(int i=n-1;i>=0;i--){
+            if(mat[i][m-1]=='O'&&vis[i][m-1]==0){
+                dfs(mat,vis,i,m-1);
+            }
+        }
+        for(int i=m-1;i>=0;i--){
+            if(mat[0][i]=='O'&&vis[0][i]==0){
+                dfs(mat,vis,0,i);
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]=='O'&&vis[i][j]==0)
+                mat[i][j]='X';
+            }
+        }
         return mat;
     }
 };
