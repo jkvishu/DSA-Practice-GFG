9c9f9aed96dfd052dd0b708e8703d2fc46c3228d
Vishu Tyagi <www.vishutyagi8@gmail.com>
2024-04-24 20:31:48 +0530

    Added Solution - From GFG

---
+++ Medium/Paths to reach origin/paths-to-reach-origin.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    int mod=1e9+7;
+    int find(int x,int y,vector<vector<int>>&dp){
+        if(x<0||y<0)
+        return 0;
+        if(x==0&&y==0)
+        return 1;
+        if(dp[x][y]!=-1)
+        return dp[x][y];
+        return dp[x][y]=(find(x-1,y,dp)%mod+find(x,y-1,dp)%mod)%mod;
+    }
+    int ways(int x, int y)
+    {
+        //code here
+        //POTD
+        //✅😏💯
+        vector<vector<int>>dp(x+1,vector<int>(y+1,-1));
+        return find(x,y,dp)%mod;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x,y;
+        cin>>x>>y;
+        Solution ob;
+        cout<<ob.ways(x,y)<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
