ad71bc8c49656f9508bc4ebd3470b57d92160c86
Vishu Tyagi <www.vishutyagi8@gmail.com>
2024-05-18 21:28:37 +0530

    Updated Solution - From GFG

--- Easy/Find the Highest number/find-the-highest-number.cpp
+++ Easy/Find the Highest number/find-the-highest-number.cpp
@@ -9,17 +9,30 @@ public:
     {
         // Code here.
         // ✅😏💯
-        int l,h=a.size()-1,m;
-        while(l<h){
-            m=(l+h)/2;
-            if(a[m]<=a[l]){
-                h=m;
+        // POTD
+        int s = 0;
+        int n = a.size();
+        int e = a.size() - 1;
+        
+        int maxi = INT_MIN;
+        while(s <= e){
+            int mid = (s+e)/2;
+            
+            if(mid+1 < n && a[mid] < a[mid+1]){
+                maxi = max(maxi,a[mid+1]);
+                s = mid+1;
+            }
+            else if(mid-1 >= 0 && a[mid-1] > a[mid]){
+                maxi = max(maxi,a[mid-1]);
+                e = mid-1;
             }
             else{
-                l=m;
+                maxi = max(maxi,a[mid]);
+                break;
             }
+            
         }
-        return a[m];
+        return maxi;
     }
 };
 
