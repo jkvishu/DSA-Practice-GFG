68f7413206c68794e91df96601ab9599cf7487c2
Vishu Tyagi <www.vishutyagi8@gmail.com>
2024-07-26 18:03:10 +0530

    Added Solution - From GFG

---
+++ Difficulty: Easy/K-Pangrams/kpangrams.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+
+    bool kPangram(string str, int k) {
+        // code here
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        cin.ignore();
+        string str;
+        getline(cin, str);
+
+        int k;
+        cin >> k;
+
+        Solution ob;
+        bool a = ob.kPangram(str, k);
+        if (a)
+            cout << "true" << endl;
+        else
+            cout << "false" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
