81deb2ff9c2aa3d71c7b10eba6ab5582a2212e75
Vishu Tyagi <www.vishutyagi8@gmail.com>
2024-09-07 21:55:44 +0530

    Updated Solution - From GFG

---
+++ Difficulty: Medium/Nth Natural Number/nth-natural-number.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    long long findNth(long long N) {
+        // code here.
+        // ©🤝👀
+        long long ans=0,m=1;
+        while (N) {
+            ans += (N%9) * m;
+            N /= 9;
+            m*= 10;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n, ans;
+        cin >> n;
+        Solution obj;
+        ans = obj.findNth(n);
+        cout << ans << endl;
+    }
+}
+
+// } Driver Code Ends
